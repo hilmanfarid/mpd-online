730754071fad1f497ef9366e6e08935780bb7c4e
haxbox.keren <haxbox.keren@gmail.com>
2013-12-31 04:57:31 +0000

    ''

--- report/cetak_surat_pengukuhan_pdf.php
+++ report/cetak_surat_pengukuhan_pdf.php
@@ -126,7 +126,7 @@ class FormCetak extends FPDF {
 		$this->Cell($formLen2 - $twelfth1, $this->height, $data["company_owner"], 0, 0, 'L');
 		$this->Ln();
 		$this->SetFont('Times', 'B', 11);
-		$this->Cell($formLen1, $this->height, "Pajak", 0, 0, 'L');
+		$this->Cell($formLen1, $this->height, "  Pajak", 0, 0, 'L');
 		
 		// Form 2. Nama Badan/Perusahaan
 		$this->Ln();
@@ -145,7 +145,7 @@ class FormCetak extends FPDF {
 		$this->Cell($formLen2 - $twelfth1, $this->height, $data["npwpd"], 0, 0, 'L');
 		$this->Ln();
 		$this->SetFont('Times', 'B', 11);
-		$this->Cell($formLen1, $this->height, "(NPWPD)", 0, 0, 'L');
+		$this->Cell($formLen1, $this->height, "  (NPWPD)", 0, 0, 'L');
 		
 		// Form 4. Alamat Tempat Tinggal
 		$this->Ln();
