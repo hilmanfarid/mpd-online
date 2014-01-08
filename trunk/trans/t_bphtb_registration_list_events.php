b4f10e2a65cda7bed1cd6567adb6e04dadf332c1
haxbox.keren@gmail.com <haxbox.keren@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-01-08 02:52:28 +0000

    ''

--- trunk/trans/t_bphtb_registration_list_events.php
+++ trunk/trans/t_bphtb_registration_list_events.php
@@ -89,9 +89,26 @@ function Page_BeforeShow(& $sender)
     // Write your own code here.
 	if(CCGetFromGet("submit_bphtb")==1){
 		$dbConn = new clsDBConnSIKP();
-		$sql="select sikp.f_first_submit_engine(505,".CCGetFromGet('t_customer_order_id').",'".CCGetSession('UserLogin')."')";
+		$sql="select count(*) as jml from t_product_order_control where doc_id = ".CCGetFromGet('t_customer_order_id')."and and p_w_doc_type_id = 505";
 		$dbConn->query($sql);
+		$jumlah_data;
+		if ($dbConn->next_record()){
+            $jumlah_data = $dbConn->f('jml');
+        }
+		if($jumlah_data==0){
+			$sql="select sikp.f_first_submit_engine(505,".CCGetFromGet('t_customer_order_id').",'".CCGetSession('UserLogin')."')";
+			$dbConn->query($sql);
+		}else{
+			echo "
+				<script>
+					alert('Data BPHTB Sudah Tersubmit');
+				</script>
+			";
+			//exit;
+		}
 		$dbConn->close();
+		header('Location:t_bphtb_registration_list.php');
+		exit;
 	}
 // -------------------------
 //End Custom Code
