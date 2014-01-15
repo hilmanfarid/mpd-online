946386ed8e1fbf68fb61785ba52030da6e831f6a
ikmal.s.16 <ikmal.s.16@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-01-15 15:07:02 +0000

    format

--- trunk/report/cetak_duplikat_kwitansi_bphtb.php
+++ trunk/report/cetak_duplikat_kwitansi_bphtb.php
@@ -31,15 +31,27 @@ class FormCetak extends FPDF {
 	
 	function PageCetak($data) {
 		$this->AddPage();
-		$this->SetFont('Courier', '', 10);
 		// $this->Cell(0, $this->height, $data["f_bphtb_receipt_duplicate"], 0, 0, "L");
 		
-		$this->Ln(30);
+		$this->Ln(20);
+		$this->SetFont('Courier', '', 14);
+		$this->Cell(180, $this->height, "", "T", 0, "C");
+		$this->Ln();
+		$this->Cell(180, $this->height, "BUKTI PEMBAYARAN BPHTB", "", 0, "C");
+		$this->Ln();
+		$this->Cell(180, $this->height, "", "B", 0, "C");
+		$this->Ln();
+		$this->Ln();
+		$this->Ln();
+		
+		$this->SetFont('Courier', '', 10);
 		$data = explode("\n", $data["f_bphtb_receipt_duplicate"]);
 		foreach($data as $datum){
 			$this->Cell(241.3, $this->height, $datum, 0, 0, "L");
 			$this->Ln();
 		}
+
+		$this->Cell(180, $this->height, "", "B", 0, "C");
 	}
 	
 	function Footer() {
