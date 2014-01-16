a71dc3f527905e9a77995032cbbeaf3403422872
haxbox.keren <haxbox.keren@gmail.com>
2014-01-16 04:48:03 +0000

    ''

--- trans/t_target_realisasi_jenis.php
+++ trans/t_target_realisasi_jenis.php
@@ -42,7 +42,7 @@ class clsGridt_target_realisasi_jenisGrid { //t_target_realisasi_jenisGrid class
     var $RowControls;
 //End Variables
 
-//Class_Initialize Event @2-072C3102
+//Class_Initialize Event @2-66A0DC00
     function clsGridt_target_realisasi_jenisGrid($RelativePath, & $Parent)
     {
         global $FileName;
@@ -59,7 +59,7 @@ class clsGridt_target_realisasi_jenisGrid { //t_target_realisasi_jenisGrid class
         $this->ds = & $this->DataSource;
         $this->PageSize = CCGetParam($this->ComponentName . "PageSize", "");
         if(!is_numeric($this->PageSize) || !strlen($this->PageSize))
-            $this->PageSize = 5;
+            $this->PageSize = 7;
         else
             $this->PageSize = intval($this->PageSize);
         if ($this->PageSize > 100)
