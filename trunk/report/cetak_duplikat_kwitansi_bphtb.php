741db0ba481ab56a85f15caf57c116aeba0df358
wiliamdecosta@gmail.com <wiliamdecosta@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-01-27 04:25:18 +0000

    ''

--- trunk/report/cetak_duplikat_kwitansi_bphtb.php
+++ trunk/report/cetak_duplikat_kwitansi_bphtb.php
@@ -30,18 +30,58 @@ class FormCetak extends FPDF {
 	}
 	
 	function PageCetak($data) {
+
 		$this->AddPage();
 		// $this->Cell(0, $this->height, $data["f_bphtb_receipt_duplicate"], 0, 0, "L");
 		
-		$this->Ln(20);
-		$this->SetFont('Courier', '', 14);
-		$this->Cell(180, $this->height, "", "T", 0, "C");
+		$exp_data = explode("\n", $data["f_bphtb_receipt_duplicate"]); //get no kuitansi
+		$exp_data1 = explode(":", $exp_data[0]);
+	
+		$encImageData = '';
+		$dbConn = new clsDBConnSIKP();
+		$query = "select f_encrypt_str('".trim($exp_data1[1])."') AS enc_data";
+
+		$dbConn->query($query);
+		while ($dbConn->next_record()) {
+			$encImageData = $dbConn->f("enc_data");
+		}
+		$dbConn->close();
+		
+		$this->Image('../images/logo_pemda.png',37,13,25,25);
+		$this->Image('http://'.$_SERVER['HTTP_HOST'].'/mpd/include/qrcode/generate-qr.php?param='.encImageData,179,13,25,25,'PNG');
+		//$this->Image('http://172.16.20.2:81/mpd/include/qrcode/generate-qr.php?param='.encImageData,179,13,25,25,'PNG');
+		
+
+		$this->SetFont('Arial', '', 11);
+
+		$this->Cell(40, $this->height, "", "", 0, 'L');
+		$this->Cell(101, $this->height, "PEMERINTAH KOTA BANDUNG", "", 0, 'C');
+		$this->Cell(40, $this->height, "", "", 0, 'L');
 		$this->Ln();
-		$this->Cell(180, $this->height, "BUKTI PEMBAYARAN BPHTB", "", 0, "C");
+		$this->Cell(40, $this->height, "", "", 0, 'L');
+		$this->Cell(101, $this->height, "DINAS PELAYANAN PAJAK", "", 0, 'C');
+		$this->Cell(40, $this->height, "", "", 0, 'L');
 		$this->Ln();
-		$this->Cell(180, $this->height, "", "B", 0, "C");
+		$this->Cell(40, $this->height, "", "", 0, 'L');
+		$this->Cell(101, $this->height, "Jalan Wastukancana No 2", "", 0, 'C');
+		$this->Cell(40, $this->height, "", "", 0, 'L');
 		$this->Ln();
+		$this->Cell(40, $this->height, "", "", 0, 'L');
+		$this->Cell(101, $this->height, "Telp. 022 4235052 - Bandung", "", 0, 'C');
+		$this->Cell(40, $this->height, "", "", 0, 'L');
 		$this->Ln();
+
+		$this->SetFont('Arial', 'B', 14);
+		$this->Cell(40, $this->height + 7, "", "B", 0, 'L');
+		$this->Cell(101, $this->height + 7, "Bukti Pembayaran Pajak BPHTB", "B", 0, 'C');
+		$this->Cell(40, $this->height + 7, "", "B", 0, 'L');
+	
+		//$this->Cell(180, $this->height, "", "T", 0, "C");
+		//$this->Ln();
+		//$this->Cell(180, $this->height, "BUKTI PEMBAYARAN BPHTB", "", 0, "C");
+		//$this->Ln();
+		//$this->Cell(180, $this->height, "", "B", 0, "C");
+		$this->Ln(5);
 		$this->Ln();
 		
 		$this->SetFont('Courier', '', 10);
