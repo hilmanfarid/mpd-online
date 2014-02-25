1761b0fb3d4af95dc9eccdd5576a65855b60683f
haxbox.keren <haxbox.keren@gmail.com>
2014-02-25 05:14:48 +0000

    ''

---
+++ services/getPembayaranBolong.ccp
@@ -0,0 +1,78 @@
+<Page id="1" templateExtension="html" relativePath=".." fullRelativePath=".\services" secured="False" urlType="Relative" isIncluded="False" SSLAccess="False" isService="True" cachingEnabled="False" cachingDuration="1 minutes" needGeneration="0">
+	<Components>
+		<Grid id="2" secured="False" sourceType="SQL" returnValueType="Number" defaultPageSize="10" connection="ConnSIKP" activeCollection="SQLParameters" parameterTypeListName="ParameterTypeList" dataSource="SELECT x.receipt_no,
+x.p_finance_period_id,
+x.description,
+f_per.*
+FROM p_finance_period f_per,
+(select sett.p_finance_period_id,
+receipt_no,sett_type.description
+						from
+							t_vat_setllement sett,
+							p_settlement_type sett_type,
+t_payment_receipt rec
+							WHERE sett.t_cust_account_id = {t_cust_account_id}
+							and sett.p_settlement_type_id = sett_type.p_settlement_type_id
+							and sett.t_vat_setllement_id = rec.t_vat_setllement_id (+)
+							and sett.p_settlement_type_id &lt;&gt; 7) as x
+					where f_per.p_finance_period_id = x.p_finance_period_id(+)
+					and f_per.end_date &lt; (select start_date from p_finance_period where p_finance_period_id = {p_finance_period_id})
+					and f_per.start_date &gt;= '01-01-2013'
+					and (receipt_no is null or receipt_no ='')
+					order by f_per.start_date" name="SELECT_x_receipt_no_x_p_f" pageSizeLimit="100" wizardCaption="List of SELECT X Receipt No,x P Finance Period Id,f Per *
+			 FROM P Finance Period F Per,
+            (select Sett P Finance Period Id,receipt No
+						 From
+							t Vat Setllement Sett,t Payment Receipt Rec
+							 WHERE Sett T Cust Account Id = {t Cust Account Id}
+							and Sett T Vat Setllement Id = Rec T Vat Setllement Id (+)
+							and Sett P Settlement Type Id &lt;&gt; 7) As X
+					where F Per P Finance Period Id = X P Finance Period Id(+)
+					and F Per End Date &lt; (select Start Date From P Finance Period Where P Finance Period Id = {p Finance Period Id})
+					and F Per Start Date &gt;= '01-01-2013'
+					and (receipt No Is Null Or Receipt No ='')
+					order By F Per Start Date " wizardAllowInsert="False">
+			<Components>
+				<Label id="178" fieldSourceType="DBColumn" dataType="Float" html="False" name="p_finance_period_id" fieldSource="p_finance_period_id">
+					<Components/>
+					<Events/>
+					<Attributes/>
+					<Features/>
+				</Label>
+				<Label id="184" fieldSourceType="DBColumn" dataType="Text" html="False" name="description" fieldSource="description">
+					<Components/>
+					<Events/>
+					<Attributes/>
+					<Features/>
+				</Label>
+				<Label id="185" fieldSourceType="DBColumn" dataType="Text" html="False" name="code" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="getPembayaranBolongSELECT_x_receipt_no_x_p_fcode" fieldSource="code">
+<Components/>
+<Events/>
+<Attributes/>
+<Features/>
+</Label>
+</Components>
+			<Events/>
+			<TableParameters/>
+			<JoinTables/>
+			<JoinLinks/>
+			<Fields/>
+			<SPParameters/>
+			<SQLParameters>
+				<SQLParameter id="175" variable="p_finance_period_id" parameterType="URL" dataType="Text" parameterSource="p_finance_period_id" designDefaultValue="190"/>
+				<SQLParameter id="176" variable="t_cust_account_id" parameterType="URL" dataType="Text" parameterSource="t_cust_account_id" designDefaultValue="100"/>
+			</SQLParameters>
+			<SecurityGroups/>
+			<Attributes/>
+			<Features/>
+		</Grid>
+	</Components>
+	<CodeFiles>
+		<CodeFile id="Code" language="PHPTemplates" name="getPembayaranBolong.php" forShow="True" url="getPembayaranBolong.php" comment="//" codePage="windows-1252"/>
+	</CodeFiles>
+	<SecurityGroups/>
+	<CachingParameters/>
+	<Attributes/>
+	<Features/>
+	<Events/>
+</Page>
---
+++ services/getPembayaranBolong.php
@@ -0,0 +1,381 @@
+<?php
+//Include Common Files @1-BDF50F88
+define("RelativePath", "..");
+define("PathToCurrentPage", "/services/");
+define("FileName", "getPembayaranBolong.php");
+include_once(RelativePath . "/Common.php");
+include_once(RelativePath . "/Template.php");
+include_once(RelativePath . "/Sorter.php");
+include_once(RelativePath . "/Navigator.php");
+//End Include Common Files
+
+class clsGridSELECT_x_receipt_no_x_p_f { //SELECT_x_receipt_no_x_p_f class @2-B1486A93
+
+//Variables @2-AC1EDBB9
+
+    // Public variables
+    var $ComponentType = "Grid";
+    var $ComponentName;
+    var $Visible;
+    var $Errors;
+    var $ErrorBlock;
+    var $ds;
+    var $DataSource;
+    var $PageSize;
+    var $IsEmpty;
+    var $ForceIteration = false;
+    var $HasRecord = false;
+    var $SorterName = "";
+    var $SorterDirection = "";
+    var $PageNumber;
+    var $RowNumber;
+    var $ControlsVisible = array();
+
+    var $CCSEvents = "";
+    var $CCSEventResult;
+
+    var $RelativePath = "";
+    var $Attributes;
+
+    // Grid Controls
+    var $StaticControls;
+    var $RowControls;
+//End Variables
+
+//Class_Initialize Event @2-A55EED7E
+    function clsGridSELECT_x_receipt_no_x_p_f($RelativePath, & $Parent)
+    {
+        global $FileName;
+        global $CCSLocales;
+        global $DefaultDateFormat;
+        $this->ComponentName = "SELECT_x_receipt_no_x_p_f";
+        $this->Visible = True;
+        $this->Parent = & $Parent;
+        $this->RelativePath = $RelativePath;
+        $this->Errors = new clsErrors();
+        $this->ErrorBlock = "Grid SELECT_x_receipt_no_x_p_f";
+        $this->Attributes = new clsAttributes($this->ComponentName . ":");
+        $this->DataSource = new clsSELECT_x_receipt_no_x_p_fDataSource($this);
+        $this->ds = & $this->DataSource;
+        $this->PageSize = CCGetParam($this->ComponentName . "PageSize", "");
+        if(!is_numeric($this->PageSize) || !strlen($this->PageSize))
+            $this->PageSize = 10;
+        else
+            $this->PageSize = intval($this->PageSize);
+        if ($this->PageSize > 100)
+            $this->PageSize = 100;
+        if($this->PageSize == 0)
+            $this->Errors->addError("<p>Form: Grid " . $this->ComponentName . "<br>Error: (CCS06) Invalid page size.</p>");
+        $this->PageNumber = intval(CCGetParam($this->ComponentName . "Page", 1));
+        if ($this->PageNumber <= 0) $this->PageNumber = 1;
+
+        $this->p_finance_period_id = & new clsControl(ccsLabel, "p_finance_period_id", "p_finance_period_id", ccsFloat, "", CCGetRequestParam("p_finance_period_id", ccsGet, NULL), $this);
+        $this->description = & new clsControl(ccsLabel, "description", "description", ccsText, "", CCGetRequestParam("description", ccsGet, NULL), $this);
+        $this->code = & new clsControl(ccsLabel, "code", "code", ccsText, "", CCGetRequestParam("code", ccsGet, NULL), $this);
+    }
+//End Class_Initialize Event
+
+//Initialize Method @2-90E704C5
+    function Initialize()
+    {
+        if(!$this->Visible) return;
+
+        $this->DataSource->PageSize = & $this->PageSize;
+        $this->DataSource->AbsolutePage = & $this->PageNumber;
+        $this->DataSource->SetOrder($this->SorterName, $this->SorterDirection);
+    }
+//End Initialize Method
+
+//Show Method @2-961F280C
+    function Show()
+    {
+        global $Tpl;
+        global $CCSLocales;
+        if(!$this->Visible) return;
+
+        $this->RowNumber = 0;
+
+        $this->DataSource->Parameters["urlp_finance_period_id"] = CCGetFromGet("p_finance_period_id", NULL);
+        $this->DataSource->Parameters["urlt_cust_account_id"] = CCGetFromGet("t_cust_account_id", NULL);
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeSelect", $this);
+
+
+        $this->DataSource->Prepare();
+        $this->DataSource->Open();
+        $this->HasRecord = $this->DataSource->has_next_record();
+        $this->IsEmpty = ! $this->HasRecord;
+        $this->Attributes->Show();
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeShow", $this);
+        if(!$this->Visible) return;
+
+        $GridBlock = "Grid " . $this->ComponentName;
+        $ParentPath = $Tpl->block_path;
+        $Tpl->block_path = $ParentPath . "/" . $GridBlock;
+
+
+        if (!$this->IsEmpty) {
+            $this->ControlsVisible["p_finance_period_id"] = $this->p_finance_period_id->Visible;
+            $this->ControlsVisible["description"] = $this->description->Visible;
+            $this->ControlsVisible["code"] = $this->code->Visible;
+            while ($this->ForceIteration || (($this->RowNumber < $this->PageSize) &&  ($this->HasRecord = $this->DataSource->has_next_record()))) {
+                // Parse Separator
+                if($this->RowNumber) {
+                    $this->Attributes->Show();
+                    $Tpl->parseto("Separator", true, "Row");
+                }
+                $this->RowNumber++;
+                if ($this->HasRecord) {
+                    $this->DataSource->next_record();
+                    $this->DataSource->SetValues();
+                }
+                $Tpl->block_path = $ParentPath . "/" . $GridBlock . "/Row";
+                $this->p_finance_period_id->SetValue($this->DataSource->p_finance_period_id->GetValue());
+                $this->description->SetValue($this->DataSource->description->GetValue());
+                $this->code->SetValue($this->DataSource->code->GetValue());
+                $this->Attributes->SetValue("rowNumber", $this->RowNumber);
+                $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeShowRow", $this);
+                $this->Attributes->Show();
+                $this->p_finance_period_id->Show();
+                $this->description->Show();
+                $this->code->Show();
+                $Tpl->block_path = $ParentPath . "/" . $GridBlock;
+                $Tpl->parse("Row", true);
+            }
+        }
+
+        $errors = $this->GetErrors();
+        if(strlen($errors))
+        {
+            $Tpl->replaceblock("", $errors);
+            $Tpl->block_path = $ParentPath;
+            return;
+        }
+        $Tpl->parse();
+        $Tpl->block_path = $ParentPath;
+        $this->DataSource->close();
+    }
+//End Show Method
+
+//GetErrors Method @2-8BF2C6E4
+    function GetErrors()
+    {
+        $errors = "";
+        $errors = ComposeStrings($errors, $this->p_finance_period_id->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->description->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->code->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->DataSource->Errors->ToString());
+        return $errors;
+    }
+//End GetErrors Method
+
+} //End SELECT_x_receipt_no_x_p_f Class @2-FCB6E20C
+
+class clsSELECT_x_receipt_no_x_p_fDataSource extends clsDBConnSIKP {  //SELECT_x_receipt_no_x_p_fDataSource Class @2-B4104131
+
+//DataSource Variables @2-A91A8B6E
+    var $Parent = "";
+    var $CCSEvents = "";
+    var $CCSEventResult;
+    var $ErrorBlock;
+    var $CmdExecution;
+
+    var $CountSQL;
+    var $wp;
+
+
+    // Datasource fields
+    var $p_finance_period_id;
+    var $description;
+    var $code;
+//End DataSource Variables
+
+//DataSourceClass_Initialize Event @2-2751D637
+    function clsSELECT_x_receipt_no_x_p_fDataSource(& $Parent)
+    {
+        $this->Parent = & $Parent;
+        $this->ErrorBlock = "Grid SELECT_x_receipt_no_x_p_f";
+        $this->Initialize();
+        $this->p_finance_period_id = new clsField("p_finance_period_id", ccsFloat, "");
+        
+        $this->description = new clsField("description", ccsText, "");
+        
+        $this->code = new clsField("code", ccsText, "");
+        
+
+    }
+//End DataSourceClass_Initialize Event
+
+//SetOrder Method @2-C1FF34B8
+    function SetOrder($SorterName, $SorterDirection)
+    {
+        $this->Order = "f_per.start_date";
+        $this->Order = CCGetOrder($this->Order, $SorterName, $SorterDirection, 
+            "");
+    }
+//End SetOrder Method
+
+//Prepare Method @2-4D9805D4
+    function Prepare()
+    {
+        global $CCSLocales;
+        global $DefaultDateFormat;
+        $this->wp = new clsSQLParameters($this->ErrorBlock);
+        $this->wp->AddParameter("1", "urlp_finance_period_id", ccsText, "", "", $this->Parameters["urlp_finance_period_id"], "", false);
+        $this->wp->AddParameter("2", "urlt_cust_account_id", ccsText, "", "", $this->Parameters["urlt_cust_account_id"], "", false);
+    }
+//End Prepare Method
+
+//Open Method @2-D409C49E
+    function Open()
+    {
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeBuildSelect", $this->Parent);
+        $this->CountSQL = "SELECT COUNT(*) FROM (SELECT x.receipt_no,\n" .
+        "x.p_finance_period_id,\n" .
+        "x.description,\n" .
+        "f_per.*\n" .
+        "FROM p_finance_period f_per,\n" .
+        "(select sett.p_finance_period_id,\n" .
+        "receipt_no,sett_type.description\n" .
+        "						from\n" .
+        "							t_vat_setllement sett,\n" .
+        "							p_settlement_type sett_type,\n" .
+        "t_payment_receipt rec\n" .
+        "							WHERE sett.t_cust_account_id = " . $this->SQLValue($this->wp->GetDBValue("2"), ccsText) . "\n" .
+        "							and sett.p_settlement_type_id = sett_type.p_settlement_type_id\n" .
+        "							and sett.t_vat_setllement_id = rec.t_vat_setllement_id (+)\n" .
+        "							and sett.p_settlement_type_id <> 7) as x\n" .
+        "					where f_per.p_finance_period_id = x.p_finance_period_id(+)\n" .
+        "					and f_per.end_date < (select start_date from p_finance_period where p_finance_period_id = " . $this->SQLValue($this->wp->GetDBValue("1"), ccsText) . ")\n" .
+        "					and f_per.start_date >= '01-01-2013'\n" .
+        "					and (receipt_no is null or receipt_no ='')) cnt";
+        $this->SQL = "SELECT x.receipt_no,\n" .
+        "x.p_finance_period_id,\n" .
+        "x.description,\n" .
+        "f_per.*\n" .
+        "FROM p_finance_period f_per,\n" .
+        "(select sett.p_finance_period_id,\n" .
+        "receipt_no,sett_type.description\n" .
+        "						from\n" .
+        "							t_vat_setllement sett,\n" .
+        "							p_settlement_type sett_type,\n" .
+        "t_payment_receipt rec\n" .
+        "							WHERE sett.t_cust_account_id = " . $this->SQLValue($this->wp->GetDBValue("2"), ccsText) . "\n" .
+        "							and sett.p_settlement_type_id = sett_type.p_settlement_type_id\n" .
+        "							and sett.t_vat_setllement_id = rec.t_vat_setllement_id (+)\n" .
+        "							and sett.p_settlement_type_id <> 7) as x\n" .
+        "					where f_per.p_finance_period_id = x.p_finance_period_id(+)\n" .
+        "					and f_per.end_date < (select start_date from p_finance_period where p_finance_period_id = " . $this->SQLValue($this->wp->GetDBValue("1"), ccsText) . ")\n" .
+        "					and f_per.start_date >= '01-01-2013'\n" .
+        "					and (receipt_no is null or receipt_no ='')\n" .
+        "					 {SQL_OrderBy}";
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeExecuteSelect", $this->Parent);
+        if ($this->CountSQL) 
+            $this->RecordsCount = CCGetDBValue(CCBuildSQL($this->CountSQL, $this->Where, ""), $this);
+        else
+            $this->RecordsCount = "CCS not counted";
+        $this->query($this->OptimizeSQL(CCBuildSQL($this->SQL, $this->Where, $this->Order)));
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "AfterExecuteSelect", $this->Parent);
+    }
+//End Open Method
+
+//SetValues Method @2-DD53298D
+    function SetValues()
+    {
+        $this->p_finance_period_id->SetDBValue(trim($this->f("p_finance_period_id")));
+        $this->description->SetDBValue($this->f("description"));
+        $this->code->SetDBValue($this->f("code"));
+    }
+//End SetValues Method
+
+} //End SELECT_x_receipt_no_x_p_fDataSource Class @2-FCB6E20C
+
+//Initialize Page @1-89026620
+// Variables
+$FileName = "";
+$Redirect = "";
+$Tpl = "";
+$TemplateFileName = "";
+$BlockToParse = "";
+$ComponentName = "";
+$Attributes = "";
+
+// Events;
+$CCSEvents = "";
+$CCSEventResult = "";
+
+$FileName = FileName;
+$Redirect = "";
+$TemplateFileName = "getPembayaranBolong.html";
+$BlockToParse = "main";
+$TemplateEncoding = "CP1252";
+$ContentType = "text/html";
+$PathToRoot = "../";
+//End Initialize Page
+
+//Before Initialize @1-E870CEBC
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeInitialize", $MainPage);
+//End Before Initialize
+
+//Initialize Objects @1-0C3E796E
+$DBConnSIKP = new clsDBConnSIKP();
+$MainPage->Connections["ConnSIKP"] = & $DBConnSIKP;
+$Attributes = new clsAttributes("page:");
+$MainPage->Attributes = & $Attributes;
+
+// Controls
+$SELECT_x_receipt_no_x_p_f = & new clsGridSELECT_x_receipt_no_x_p_f("", $MainPage);
+$MainPage->SELECT_x_receipt_no_x_p_f = & $SELECT_x_receipt_no_x_p_f;
+$SELECT_x_receipt_no_x_p_f->Initialize();
+
+$CCSEventResult = CCGetEvent($CCSEvents, "AfterInitialize", $MainPage);
+
+if ($Charset) {
+    header("Content-Type: " . $ContentType . "; charset=" . $Charset);
+} else {
+    header("Content-Type: " . $ContentType);
+}
+//End Initialize Objects
+
+//Initialize HTML Template @1-52F9C312
+$CCSEventResult = CCGetEvent($CCSEvents, "OnInitializeView", $MainPage);
+$Tpl = new clsTemplate($FileEncoding, $TemplateEncoding);
+$Tpl->LoadTemplate(PathToCurrentPage . $TemplateFileName, $BlockToParse, "CP1252");
+$Tpl->block_path = "/$BlockToParse";
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeShow", $MainPage);
+$Attributes->SetValue("pathToRoot", "../");
+$Attributes->Show();
+//End Initialize HTML Template
+
+//Go to destination page @1-6E80FE88
+if($Redirect)
+{
+    $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+    $DBConnSIKP->close();
+    header("Location: " . $Redirect);
+    unset($SELECT_x_receipt_no_x_p_f);
+    unset($Tpl);
+    exit;
+}
+//End Go to destination page
+
+//Show Page @1-4D6AA5C4
+$SELECT_x_receipt_no_x_p_f->Show();
+$Tpl->block_path = "";
+$Tpl->Parse($BlockToParse, false);
+if (!isset($main_block)) $main_block = $Tpl->GetVar($BlockToParse);
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeOutput", $MainPage);
+if ($CCSEventResult) echo $main_block;
+//End Show Page
+
+//Unload Page @1-57A767AA
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+$DBConnSIKP->close();
+unset($SELECT_x_receipt_no_x_p_f);
+unset($Tpl);
+//End Unload Page
+
+
+?>
