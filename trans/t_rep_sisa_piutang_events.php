f0fcfceace50ff1cbe36e10af95cfacf6312d8be
wiliamdecosta <wiliamdecosta@gmail.com>
2014-03-24 09:45:24 +0000

    ''

---
+++ trans/t_rep_sisa_piutang_events.php
@@ -0,0 +1,165 @@
+<?php
+//BindEvents Method @1-EF623785
+function BindEvents()
+{
+    global $CCSEvents;
+    $CCSEvents["OnInitializeView"] = "Page_OnInitializeView";
+    $CCSEvents["BeforeShow"] = "Page_BeforeShow";
+}
+//End BindEvents Method
+
+//Page_OnInitializeView @1-D3A028B1
+function Page_OnInitializeView(& $sender)
+{
+    $Page_OnInitializeView = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_rep_sisa_piutang; //Compatibility
+//End Page_OnInitializeView
+
+//Custom Code @66-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close Page_OnInitializeView @1-81DF8332
+    return $Page_OnInitializeView;
+}
+//End Close Page_OnInitializeView
+
+//Page_BeforeShow @1-C8C6DB7F
+function Page_BeforeShow(& $sender)
+{
+    $Page_BeforeShow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_rep_sisa_piutang; //Compatibility
+//End Page_BeforeShow
+	global $Label1;
+	global $t_rep_sisa_piutangSearch;
+//Custom Code @566-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+	$doAction = CCGetFromGet('doAction');
+	if($doAction == 'view_html') {
+				
+		$data = array();
+		
+		$param_arr = array();
+				
+		$param_arr['p_year_period_id'] = CCGetFromGet('p_year_period_id');
+		$param_arr['p_finance_period_id'] = CCGetFromGet('p_finance_period_id');
+		$param_arr['p_vat_type_id'] = CCGetFromGet('p_vat_type_id');
+
+		$param_arr['tahun_periode'] = CCGetFromGet('tahun_periode');
+		$param_arr['pajak_periode'] = CCGetFromGet('pajak_periode');
+		$param_arr['jenis_pajak'] = CCGetFromGet('jenis_pajak');
+
+		$t_rep_sisa_piutangSearch->p_year_period_id->SetValue($param_arr['p_year_period_id']);
+		$t_rep_sisa_piutangSearch->p_finance_period_id->SetValue($param_arr['p_finance_period_id']);
+		$t_rep_sisa_piutangSearch->p_vat_type_id->SetValue($param_arr['p_vat_type_id']);
+			
+		$t_rep_sisa_piutangSearch->year_code->SetValue($param_arr['tahun_periode']);
+		$t_rep_sisa_piutangSearch->code->SetValue($param_arr['pajak_periode']);
+		$t_rep_sisa_piutangSearch->vat_code->SetValue($param_arr['jenis_pajak']);
+
+		if(!empty($param_arr['p_finance_period_id']) and !empty($param_arr['p_vat_type_id'])) {
+			
+			$dbConn	= new clsDBConnSIKP();
+			$query="SELECT * FROM f_rep_status_piutang (".$param_arr['p_vat_type_id'].", ".$param_arr['p_finance_period_id'].", 1)";
+			
+			$data = array();
+			$dbConn->query($query);
+			while ($dbConn->next_record()) {
+				$data[] = $dbConn->Record;
+			}
+
+			$dbConn->close();
+			$Label1->SetText(GetCetakHTML($data, $param_arr['pajak_periode'], $param_arr['jenis_pajak']));
+
+		}else {
+			/* Tampilkan Alert */
+			echo '<script> alert("Semua Filter Harus Diisi"); </script>';
+		}
+	}
+	else {
+		
+		//do nothing 
+	}
+	
+//Close Page_BeforeShow @1-4BC230CD
+    return $Page_BeforeShow;
+}
+//End Close Page_BeforeShow
+
+function GetCetakHTML($data, $pajak_periode, $jenis_pajak) {
+
+	$output = '';
+	
+	$output .='<table id="table-piutang" class="grid-table-container" border="0" cellspacing="0" cellpadding="0" width="100%">
+          		<tr>
+            		<td valign="top">';
+
+	$output .='<table class="grid-table" border="0" cellspacing="0" cellpadding="0">
+                	<tr>
+                  		<td class="HeaderLeft"><img border="0" alt="" src="../Styles/sikp/Images/Spacer.gif"></td> 
+                  		<td class="th"><strong>LAPORAN STATUS PIUTANG</strong></td> 
+                  		<td class="HeaderRight"><img border="0" alt="" src="../Styles/sikp/Images/Spacer.gif"></td>
+                	</tr>
+              		</table>';
+	
+	$output .= '<h2>JENIS PAJAK : '.$jenis_pajak.' </h2>';
+	$output .= '<h2>JATUH TEMPO : '.$pajak_periode.'</h2> <br/>';
+
+	$output .='<table id="table-piutang-detil" class="Grid" border="1" cellspacing="0" cellpadding="3px">
+                <tr >';
+
+
+		$output.='<th align="center" rowspan="2">NO</th>';
+		$output.='<th align="center" rowspan="2">WAJIB PAJAK</th>';
+		$output.='<th align="center" rowspan="2">NPWPD</th>';
+		$output.='<th align="center" rowspan="2">SPTPD</th>';
+		$output.='<th align="center" rowspan="2">STPD</th>';
+		$output.='<th align="center" colspan="2">TEGURAN I <br/> '.$data[0]['f_teg1_sts'].'</th>';
+		$output.='<th align="center" colspan="2">TEGURAN II <br/> '.$data[0]['f_teg2_sts'].'</th>';
+		$output.='<th align="center" colspan="2">TEGURAN III <br/> '.$data[0]['f_teg3_sts'].'</th>';
+		$output.='<th align="center" rowspan="2">AKSI</th>';
+		$output.='</tr>';
+    	
+		$output.='<tr >';
+		$output.='<th align="center">SPTPD</th>';
+		$output.='<th align="center">STPD</th>';
+		$output.='<th align="center">SPTPD</th>';
+		$output.='<th align="center">STPD</th>';
+		$output.='<th align="center">SPTPD</th>';
+		$output.='<th align="center">STPD</th>';
+		$output.='</tr>';
+    	
+		for ($i = 0; $i < count($data); $i++) {
+
+			$output .= '<tr>';
+			$output .= '<td align="center">'.($i+1).'</td>';
+			$output .= '<td align="left">'.$data[$i]['nama'].'</td>';
+			$output .= '<td align="center">'.$data[$i]['npwpd'].'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_amout'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_penalty'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg1_amount'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg1_penalty'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg2_amount'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg2_penalty'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg3_amount'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['f_teg3_penalty'],0,",",".").'</td>';
+			$output .= '<td align="right">'.number_format($data[$i]['jml_piutang'],0,",",".").'</td>';
+			
+			$output .= '</tr>';
+		}
+	
+	$output.='</td></tr></table>';
+	$output.='</table>';
+	
+	return $output;
+}
+?>
