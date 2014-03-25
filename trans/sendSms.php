92cf57daf9dc9640dac713f0827e1c5719dbccf6
haxbox.keren <haxbox.keren@gmail.com>
2014-03-25 05:19:32 +0000

    ''

---
+++ trans/sendSms.php
@@ -0,0 +1,37 @@
+<html>
+    <form action='../include/excel/curl_tes.php' method=post>
+        <table>
+            <tr>
+                <td>
+                    NPWD :
+                </td>
+                <td>
+                    <input type="text" name="npwd" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    No Telp :
+                </td>
+                <td>
+                    <input type="text" name="no_telp" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Pesan :
+                </td>
+                <td>
+                    <textarea name="message">
+                    </textarea>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" value="Kirim Pesan"/>
+                </td>
+            </tr>
+            
+        </table>
+    </form>    
+</html>
