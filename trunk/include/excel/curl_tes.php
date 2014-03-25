d807fbf5ec5f0f34a2f08efdfc38a33b76a8261d
haxbox.keren@gmail.com <haxbox.keren@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-03-25 05:21:46 +0000

    ''

---
+++ trunk/include/excel/curl_tes.php
@@ -0,0 +1,45 @@
+<?php
+function sendSms(){
+    include 'save_excel.php';
+    $npwd = $_GET['npwd'];
+    $no_telp = $_GET['no_telp'];
+    $message = $_GET['message'];
+    $file_name = createExcel($no_telp,$npwd);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/_libz/usersignin.php");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    // in real life you should use something like:
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 
+                    array(  'loginUsername'  => 'radnettrial',
+                            'loginPass'    => 'trial',
+                            'attached_type' => 'action_1',
+                            'senderID'      => 'JMP000003',
+                            'sender'	    => 'INFO',
+                            'tanggal'	    => '2014-03-11',
+                            'jam1'	        => '16',
+                            'mnt1'	        => '19',
+                            'pesan'	        => $message,
+                            'tb_simpan'	    => 'Submit',
+                            'login_btn'     => 'Login',
+                            'nmbatch'       => '@' . realpath($file_name.'.xls') . ';filename=send_sms.xls'
+                          )
+                    );
+    
+    // receive server response ...
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
+    
+    $tmp_fname = tempnam("/tmp", "COOKIE");
+     
+    curl_setopt ($ch, CURLOPT_COOKIEJAR, $tmp_fname);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    $server_output = curl_exec ($ch);
+    
+    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/userarea/p/single_exe.php");
+    curl_setopt ($ch, CURLOPT_COOKIEFILE, $tmp_fname);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    $server_output = curl_exec ($ch);
+    curl_close ($ch);
+}
+sendSms
---
+++ trunk/include/excel/save_excel.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * PHPExcel
+ *
+ * Copyright (C) 2006 - 2014 PHPExcel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category   PHPExcel
+ * @package    PHPExcel
+ * @copyright  Copyright (c) 2006 - 2014 PHPExcel (http://www.codeplex.com/PHPExcel)
+ * @license    http://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt	LGPL
+ * @version    1.8.0, 2014-03-02
+ */
+
+/** Error reporting */
+error_reporting(E_ALL);
+function createExcel($no_telp,$npwd){
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+    date_default_timezone_set('Europe/London');
+    
+    define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+    
+    /** Include PHPExcel */
+    require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
+    
+    
+    // Create new PHPExcel object
+    $objPHPExcel = new PHPExcel();
+    
+    // Set document properties
+    $objPHPExcel->getProperties()->setCreator("Disyanjak Bandung")
+    							 ->setLastModifiedBy("Disyanjak Bandung")
+    							 ->setTitle("Daftar SMS")
+    							 ->setSubject("Daftar SMS")
+    							 ->setDescription("Daftar SMS untuk ke WP")
+    							 ->setKeywords("office PHPExcel php")
+    							 ->setCategory("Test result file");
+    
+    $objPHPExcel->getActiveSheet()->getStyle('A2')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER);
+    // Add some data
+    $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue('A1', 'Phone No.')
+                ->setCellValue('A2', $no_telp);
+                
+    //$objPHPExcel->getActiveSheet()->getStyle('A1')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+    
+    
+    //$objPHPExcel->getActiveSheet()->setCellValue('A8',"Hello\nWorld");
+    //$objPHPExcel->getActiveSheet()->getRowDimension(8)->setRowHeight(-1);
+    //$objPHPExcel->getActiveSheet()->getStyle('A8')->getAlignment()->setWrapText(true);
+    
+    
+    // Rename worksheet
+    $objPHPExcel->getActiveSheet()->setTitle('Daftar SMS');
+    
+    
+    // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+    $objPHPExcel->setActiveSheetIndex(0);
+    
+    
+    // Save Excel 2007 file
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $fileName = 'send_sms_'.$npwd;
+    $objWriter->save($fileName.'.xlsx');
+    
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+    $objWriter->save($fileName.'.xls');
+    
+    return $fileName;
+}
