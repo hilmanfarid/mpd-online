72540660b43936882003ba2d00d64e31f6f5b42a
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-04-29 03:36:49 +0000

    ''

--- trunk/report/cetak_rep_bphtb.php
+++ trunk/report/cetak_rep_bphtb.php
@@ -321,10 +321,10 @@ class FormCetak extends FPDF {
 		$this->newLine();				
 		//$this->Cell($lbody3 - 10, $this->height, "", "", 0, 'L');
 		//$this->Cell($lbody1 + 10, $this->height, "(....................................)", "", 0, 'C');
-		$this->Cell($lbody1 + 10, $this->height - 4, "(INDRA WISNU, SE.)", "", 0, 'C');
+		$this->Cell($lbody1 + 10, $this->height - 4, "(ZAENAL MANSUR, SE.)", "", 0, 'C');
 		$this->Cell(202, $this->height - 4, "( ".$data['verificated_by']." )", "", 0, 'C');
 		$this->newLine();
-		$this->Cell($lbody1 + 10, $this->height - 4, "NIP : 19731031.2009.1.1001 ", "", 0, 'C');
+		$this->Cell($lbody1 + 10, $this->height - 4, "NIP : 19630817.1989.01.1.006 ", "", 0, 'C');
 		$this->Cell(202, $this->height - 4, "NIP : ".$data['verificated_nip']." ", "", 0, 'C');
 
 			
