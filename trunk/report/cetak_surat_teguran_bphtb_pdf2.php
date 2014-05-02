bc8c6bb4aa638a95420e7a741c5145b07cdfbb8f
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-05-02 04:47:18 +0000

    ''

--- trunk/report/cetak_surat_teguran_bphtb_pdf2.php
+++ trunk/report/cetak_surat_teguran_bphtb_pdf2.php
@@ -417,7 +417,16 @@ class FormCetak extends FPDF {
 		
 		$this->SetWidths(array(25,170));
 		$this->RowMultiBorderWithHeight(
-			array("","Belum melakukan pembayaran pajak BPHTB Tahun ".date("Y")." dengan Nilai sebesar Rp. ".number_format($data['bphtb_amt_final'],2,",",".")." sesuai dengan : "
+			array("","Belum melakukan pembayaran pajak BPHTB Tahun ".date("Y")." dengan Pajak Terutang sebesar"
+			),
+			array("",""
+			),
+			5
+		);
+
+		$this->SetWidths(array(25,170));
+		$this->RowMultiBorderWithHeight(
+			array("","Rp. ".number_format($data['bphtb_amt_final'],2,",",".")." sesuai dengan : "
 			),
 			array("",""
 			),
