f368a000c8c4d1812bdc53dac02fa28ff7fbbcdc
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-05-02 07:27:33 +0000

    ''

--- trunk/report/cetak_formulir_surat_teguran_pdf_new_piutang.php
+++ trunk/report/cetak_formulir_surat_teguran_pdf_new_piutang.php
@@ -194,7 +194,7 @@ class FormCetak extends FPDF {
 		$lkepada3 = $lkepada * 3;
 		
 		$this->Cell($lkepada3, $this->height, "", "L", 0, 'L');
-		$this->Cell($lkepada2, $this->height, "Bandung, 23 Desember 2013", "R", 0, 'L');
+		$this->Cell($lkepada2, $this->height, "Bandung, ".dateToString(date("d-M-Y")), "R", 0, 'L');
 		$this->Ln();
 
 		$this->Cell($lkepada3, $this->height, "", "L", 0, 'L');
@@ -333,7 +333,7 @@ class FormCetak extends FPDF {
 		$this->Cell($lbody2, $this->height, "", "L", 0, 'C');
 		$this->Cell($lbody4, $this->height, "", "", 0, 'C');
 		$this->Cell($lbody4, $this->height, "", "", 0, 'C');
-		$this->Cell($lbody4, $this->height, "Bandung, 23 Desember 2013" /*. $data["tanggal"]*/, "", 0, 'C');
+		$this->Cell($lbody4, $this->height, "Bandung, ".dateToString(date("d-M-Y") /*. $data["tanggal"]*/, "", 0, 'C');
 		$this->Cell($lbody2, $this->height, "", "R", 0, 'C');
 		$this->Ln();
 		
@@ -666,7 +666,7 @@ function dateToString($date){
 	
 	$pieces = explode('-', $date);
 	
-	return $pieces[2].' '.$monthname[(int)$pieces[1]].' '.$pieces[0];
+	return $monthname[(int)$pieces[1]].' '.$pieces[0];
 }
 
 
