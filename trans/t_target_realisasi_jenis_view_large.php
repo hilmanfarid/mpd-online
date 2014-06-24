2350093712a98a00d44c2f6a3babd8f19fc9f6e2
fajar.rusius <fajar.rusius@gmail.com>
2014-06-24 04:03:34 +0000

    ''

--- trans/t_target_realisasi_jenis_view_large.php
+++ trans/t_target_realisasi_jenis_view_large.php
@@ -716,6 +716,285 @@ class clst_target_realisasi_jenisGrid1DataSource extends clsDBConnSIKP {  //t_ta
 
 } //End t_target_realisasi_jenisGrid1DataSource Class @880-FCB6E20C
 
+class clsGridt_target_realisasiGrid { //t_target_realisasiGrid class @909-7DA52549
+
+//Variables @909-AC1EDBB9
+
+    // Public variables
+    var $ComponentType = "Grid";
+    var $ComponentName;
+    var $Visible;
+    var $Errors;
+    var $ErrorBlock;
+    var $ds;
+    var $DataSource;
+    var $PageSize;
+    var $IsEmpty;
+    var $ForceIteration = false;
+    var $HasRecord = false;
+    var $SorterName = "";
+    var $SorterDirection = "";
+    var $PageNumber;
+    var $RowNumber;
+    var $ControlsVisible = array();
+
+    var $CCSEvents = "";
+    var $CCSEventResult;
+
+    var $RelativePath = "";
+    var $Attributes;
+
+    // Grid Controls
+    var $StaticControls;
+    var $RowControls;
+//End Variables
+
+//Class_Initialize Event @909-869A6057
+    function clsGridt_target_realisasiGrid($RelativePath, & $Parent)
+    {
+        global $FileName;
+        global $CCSLocales;
+        global $DefaultDateFormat;
+        $this->ComponentName = "t_target_realisasiGrid";
+        $this->Visible = True;
+        $this->Parent = & $Parent;
+        $this->RelativePath = $RelativePath;
+        $this->Errors = new clsErrors();
+        $this->ErrorBlock = "Grid t_target_realisasiGrid";
+        $this->Attributes = new clsAttributes($this->ComponentName . ":");
+        $this->DataSource = new clst_target_realisasiGridDataSource($this);
+        $this->ds = & $this->DataSource;
+        $this->PageSize = CCGetParam($this->ComponentName . "PageSize", "");
+        if(!is_numeric($this->PageSize) || !strlen($this->PageSize))
+            $this->PageSize = 5;
+        else
+            $this->PageSize = intval($this->PageSize);
+        if ($this->PageSize > 100)
+            $this->PageSize = 100;
+        if($this->PageSize == 0)
+            $this->Errors->addError("<p>Form: Grid " . $this->ComponentName . "<br>Error: (CCS06) Invalid page size.</p>");
+        $this->PageNumber = intval(CCGetParam($this->ComponentName . "Page", 1));
+        if ($this->PageNumber <= 0) $this->PageNumber = 1;
+
+        $this->year_code = & new clsControl(ccsLabel, "year_code", "year_code", ccsText, "", CCGetRequestParam("year_code", ccsGet, NULL), $this);
+        $this->target_amt = & new clsControl(ccsLabel, "target_amt", "target_amt", ccsFloat, array(False, 2, Null, Null, False, "", "", 1, True, ""), CCGetRequestParam("target_amt", ccsGet, NULL), $this);
+        $this->DLink = & new clsControl(ccsLink, "DLink", "DLink", ccsText, "", CCGetRequestParam("DLink", ccsGet, NULL), $this);
+        $this->DLink->HTML = true;
+        $this->DLink->Page = "t_target_realisasi.php";
+        $this->p_year_period_id = & new clsControl(ccsHidden, "p_year_period_id", "p_year_period_id", ccsText, "", CCGetRequestParam("p_year_period_id", ccsGet, NULL), $this);
+        $this->realisasi_amt = & new clsControl(ccsLabel, "realisasi_amt", "realisasi_amt", ccsFloat, array(False, 2, Null, Null, False, "", "", 1, True, ""), CCGetRequestParam("realisasi_amt", ccsGet, NULL), $this);
+        $this->percentage = & new clsControl(ccsLabel, "percentage", "percentage", ccsFloat, "", CCGetRequestParam("percentage", ccsGet, NULL), $this);
+        $this->Navigator = & new clsNavigator($this->ComponentName, "Navigator", $FileName, 10, tpCentered, $this);
+        $this->Navigator->PageSizes = array("1", "5", "10", "25", "50");
+        $this->p_year_period_id2 = & new clsControl(ccsHidden, "p_year_period_id2", "p_year_period_id2", ccsText, "", CCGetRequestParam("p_year_period_id2", ccsGet, NULL), $this);
+    }
+//End Class_Initialize Event
+
+//Initialize Method @909-90E704C5
+    function Initialize()
+    {
+        if(!$this->Visible) return;
+
+        $this->DataSource->PageSize = & $this->PageSize;
+        $this->DataSource->AbsolutePage = & $this->PageNumber;
+        $this->DataSource->SetOrder($this->SorterName, $this->SorterDirection);
+    }
+//End Initialize Method
+
+//Show Method @909-A5C748C9
+    function Show()
+    {
+        global $Tpl;
+        global $CCSLocales;
+        if(!$this->Visible) return;
+
+        $this->RowNumber = 0;
+
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeSelect", $this);
+
+
+        $this->DataSource->Prepare();
+        $this->DataSource->Open();
+        $this->HasRecord = $this->DataSource->has_next_record();
+        $this->IsEmpty = ! $this->HasRecord;
+        $this->Attributes->Show();
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeShow", $this);
+        if(!$this->Visible) return;
+
+        $GridBlock = "Grid " . $this->ComponentName;
+        $ParentPath = $Tpl->block_path;
+        $Tpl->block_path = $ParentPath . "/" . $GridBlock;
+
+
+        if (!$this->IsEmpty) {
+            $this->ControlsVisible["year_code"] = $this->year_code->Visible;
+            $this->ControlsVisible["target_amt"] = $this->target_amt->Visible;
+            $this->ControlsVisible["DLink"] = $this->DLink->Visible;
+            $this->ControlsVisible["p_year_period_id"] = $this->p_year_period_id->Visible;
+            $this->ControlsVisible["realisasi_amt"] = $this->realisasi_amt->Visible;
+            $this->ControlsVisible["percentage"] = $this->percentage->Visible;
+            while ($this->ForceIteration || (($this->RowNumber < $this->PageSize) &&  ($this->HasRecord = $this->DataSource->has_next_record()))) {
+                $this->RowNumber++;
+                if ($this->HasRecord) {
+                    $this->DataSource->next_record();
+                    $this->DataSource->SetValues();
+                }
+                $Tpl->block_path = $ParentPath . "/" . $GridBlock . "/Row";
+                $this->year_code->SetValue($this->DataSource->year_code->GetValue());
+                $this->target_amt->SetValue($this->DataSource->target_amt->GetValue());
+                $this->DLink->Parameters = CCGetQueryString("QueryString", array("ccsForm"));
+                $this->DLink->Parameters = CCAddParam($this->DLink->Parameters, "p_year_period_id", $this->DataSource->f("p_year_period_id"));
+                $this->p_year_period_id->SetValue($this->DataSource->p_year_period_id->GetValue());
+                $this->realisasi_amt->SetValue($this->DataSource->realisasi_amt->GetValue());
+                $this->Attributes->SetValue("rowNumber", $this->RowNumber);
+                $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeShowRow", $this);
+                $this->Attributes->Show();
+                $this->year_code->Show();
+                $this->target_amt->Show();
+                $this->DLink->Show();
+                $this->p_year_period_id->Show();
+                $this->realisasi_amt->Show();
+                $this->percentage->Show();
+                $Tpl->block_path = $ParentPath . "/" . $GridBlock;
+                $Tpl->parse("Row", true);
+            }
+        }
+        else { // Show NoRecords block if no records are found
+            $this->Attributes->Show();
+            $Tpl->parse("NoRecords", false);
+        }
+
+        $errors = $this->GetErrors();
+        if(strlen($errors))
+        {
+            $Tpl->replaceblock("", $errors);
+            $Tpl->block_path = $ParentPath;
+            return;
+        }
+        $this->Navigator->PageNumber = $this->DataSource->AbsolutePage;
+        $this->Navigator->PageSize = $this->PageSize;
+        if ($this->DataSource->RecordsCount == "CCS not counted")
+            $this->Navigator->TotalPages = $this->DataSource->AbsolutePage + ($this->DataSource->next_record() ? 1 : 0);
+        else
+            $this->Navigator->TotalPages = $this->DataSource->PageCount();
+        if ($this->Navigator->TotalPages <= 1) {
+            $this->Navigator->Visible = false;
+        }
+        $this->Navigator->Show();
+        $this->p_year_period_id2->Show();
+        $Tpl->parse();
+        $Tpl->block_path = $ParentPath;
+        $this->DataSource->close();
+    }
+//End Show Method
+
+//GetErrors Method @909-12C3946C
+    function GetErrors()
+    {
+        $errors = "";
+        $errors = ComposeStrings($errors, $this->year_code->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->target_amt->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->DLink->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->p_year_period_id->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->realisasi_amt->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->percentage->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->Errors->ToString());
+        $errors = ComposeStrings($errors, $this->DataSource->Errors->ToString());
+        return $errors;
+    }
+//End GetErrors Method
+
+} //End t_target_realisasiGrid Class @909-FCB6E20C
+
+class clst_target_realisasiGridDataSource extends clsDBConnSIKP {  //t_target_realisasiGridDataSource Class @909-9A91A27E
+
+//DataSource Variables @909-7AEB8ABC
+    var $Parent = "";
+    var $CCSEvents = "";
+    var $CCSEventResult;
+    var $ErrorBlock;
+    var $CmdExecution;
+
+    var $CountSQL;
+    var $wp;
+
+
+    // Datasource fields
+    var $year_code;
+    var $target_amt;
+    var $p_year_period_id;
+    var $realisasi_amt;
+//End DataSource Variables
+
+//DataSourceClass_Initialize Event @909-3716EEE5
+    function clst_target_realisasiGridDataSource(& $Parent)
+    {
+        $this->Parent = & $Parent;
+        $this->ErrorBlock = "Grid t_target_realisasiGrid";
+        $this->Initialize();
+        $this->year_code = new clsField("year_code", ccsText, "");
+        
+        $this->target_amt = new clsField("target_amt", ccsFloat, "");
+        
+        $this->p_year_period_id = new clsField("p_year_period_id", ccsText, "");
+        
+        $this->realisasi_amt = new clsField("realisasi_amt", ccsFloat, "");
+        
+
+    }
+//End DataSourceClass_Initialize Event
+
+//SetOrder Method @909-9E1383D1
+    function SetOrder($SorterName, $SorterDirection)
+    {
+        $this->Order = "";
+        $this->Order = CCGetOrder($this->Order, $SorterName, $SorterDirection, 
+            "");
+    }
+//End SetOrder Method
+
+//Prepare Method @909-14D6CD9D
+    function Prepare()
+    {
+        global $CCSLocales;
+        global $DefaultDateFormat;
+    }
+//End Prepare Method
+
+//Open Method @909-023FD52C
+    function Open()
+    {
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeBuildSelect", $this->Parent);
+        $this->CountSQL = "SELECT COUNT(*) FROM (select * from v_target_realisasi_updated\n" .
+        "where target_amt > 0 AND realisasi_amt > 0\n" .
+        ") cnt";
+        $this->SQL = "select * from v_target_realisasi_updated\n" .
+        "where target_amt > 0 AND realisasi_amt > 0\n" .
+        "";
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeExecuteSelect", $this->Parent);
+        if ($this->CountSQL) 
+            $this->RecordsCount = CCGetDBValue(CCBuildSQL($this->CountSQL, $this->Where, ""), $this);
+        else
+            $this->RecordsCount = "CCS not counted";
+        $this->query($this->OptimizeSQL(CCBuildSQL($this->SQL, $this->Where, $this->Order)));
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "AfterExecuteSelect", $this->Parent);
+    }
+//End Open Method
+
+//SetValues Method @909-C6209C4F
+    function SetValues()
+    {
+        $this->year_code->SetDBValue($this->f("year_code"));
+        $this->target_amt->SetDBValue(trim($this->f("target_amt")));
+        $this->p_year_period_id->SetDBValue($this->f("p_year_period_id"));
+        $this->realisasi_amt->SetDBValue(trim($this->f("realisasi_amt")));
+    }
+//End SetValues Method
+
+} //End t_target_realisasiGridDataSource Class @909-FCB6E20C
+
 //Initialize Page @1-FCC62E73
 // Variables
 $FileName = "";
@@ -748,7 +1027,7 @@ include_once("./t_target_realisasi_jenis_view_large_events.php");
 $CCSEventResult = CCGetEvent($CCSEvents, "BeforeInitialize", $MainPage);
 //End Before Initialize
 
-//Initialize Objects @1-2548330F
+//Initialize Objects @1-E862A6C0
 $DBConnSIKP = new clsDBConnSIKP();
 $MainPage->Connections["ConnSIKP"] = & $DBConnSIKP;
 $Attributes = new clsAttributes("page:");
@@ -757,10 +1036,13 @@ $MainPage->Attributes = & $Attributes;
 // Controls
 $t_target_realisasi_jenisGrid = & new clsGridt_target_realisasi_jenisGrid("", $MainPage);
 $t_target_realisasi_jenisGrid1 = & new clsGridt_target_realisasi_jenisGrid1("", $MainPage);
+$t_target_realisasiGrid = & new clsGridt_target_realisasiGrid("", $MainPage);
 $MainPage->t_target_realisasi_jenisGrid = & $t_target_realisasi_jenisGrid;
 $MainPage->t_target_realisasi_jenisGrid1 = & $t_target_realisasi_jenisGrid1;
+$MainPage->t_target_realisasiGrid = & $t_target_realisasiGrid;
 $t_target_realisasi_jenisGrid->Initialize();
 $t_target_realisasi_jenisGrid1->Initialize();
+$t_target_realisasiGrid->Initialize();
 
 BindEvents();
 
@@ -783,7 +1065,7 @@ $Attributes->SetValue("pathToRoot", "../");
 $Attributes->Show();
 //End Initialize HTML Template
 
-//Go to destination page @1-D36BBAE7
+//Go to destination page @1-8AE3560C
 if($Redirect)
 {
     $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
@@ -791,14 +1073,16 @@ if($Redirect)
     header("Location: " . $Redirect);
     unset($t_target_realisasi_jenisGrid);
     unset($t_target_realisasi_jenisGrid1);
+    unset($t_target_realisasiGrid);
     unset($Tpl);
     exit;
 }
 //End Go to destination page
 
-//Show Page @1-103084D2
+//Show Page @1-767C0A37
 $t_target_realisasi_jenisGrid->Show();
 $t_target_realisasi_jenisGrid1->Show();
+$t_target_realisasiGrid->Show();
 $Tpl->block_path = "";
 $Tpl->Parse($BlockToParse, false);
 if (!isset($main_block)) $main_block = $Tpl->GetVar($BlockToParse);
@@ -806,11 +1090,12 @@ $CCSEventResult = CCGetEvent($CCSEvents, "BeforeOutput", $MainPage);
 if ($CCSEventResult) echo $main_block;
 //End Show Page
 
-//Unload Page @1-7B2C05E4
+//Unload Page @1-A617E7D3
 $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
 $DBConnSIKP->close();
 unset($t_target_realisasi_jenisGrid);
 unset($t_target_realisasi_jenisGrid1);
+unset($t_target_realisasiGrid);
 unset($Tpl);
 //End Unload Page
 
