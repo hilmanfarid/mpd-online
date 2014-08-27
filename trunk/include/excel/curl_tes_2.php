7fd00d8a1b75b1f13b3d74c548709f74c9c178c9
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-08-27 09:41:20 +0000

    ''

--- trunk/include/excel/curl_tes_2.php
+++ trunk/include/excel/curl_tes_2.php
@@ -27,6 +27,53 @@ function sendSms(){
 
 	if(sizeof($data)>0){
 		//print_r($data);
+		$file_name = createExcel($data);
+		date_default_timezone_set('Asia/Jakarta');
+		$send_date = date('Y-m-d-H-i-s');
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/_libz/usersignin.php");
+	    curl_setopt($ch, CURLOPT_POST, 1);
+	    // in real life you should use something like:
+	        curl_setopt($ch, CURLOPT_POSTFIELDS, 
+	                    array(  'loginUsername'  => 'disyanjak',//'radnettrial',
+	                            'loginPass'    => 'disyanjakbdg12345678',//'trial',
+	                            'attached_type' => 'action_1',
+	                            'senderID'      => 'JMP000007',
+	                            'sender'	    => 'DISYANJAK',
+	                            'tanggal'	    => substr($send_date,0,10),//'2014-03-11',
+	                            'jam1'	        => substr($send_date,11,2),//'16',
+	                            'mnt1'	        => ''.(substr($send_date,14,2)+1).'',//'19',
+	                            'pesan'	        => '',
+	                            'tb_simpan'	    => 'Submit',
+	                            'login_btn'     => 'Login',
+	                            'nmbatch'       => '@' . realpath($file_name.'.xls') . ';filename='.$file_name.'_'.$send_date.'.xls'
+	                          )
+	                    );
+    
+	    // receive server response ...
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
+    
+	    $tmp_fname = tempnam("/tmp", "COOKIE");
+     
+	    curl_setopt ($ch, CURLOPT_COOKIEJAR, $tmp_fname);
+	    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    
+	    $server_output = curl_exec ($ch);
+    
+	    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/userarea/p/personalize_exe.php");
+	    curl_setopt ($ch, CURLOPT_COOKIEFILE, $tmp_fname);
+	    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+	    $server_output = curl_exec ($ch);
+		echo $server_output;
+	    curl_close ($ch);
+
+		for($counter = 0;$counter < sizeof($data);$counter++){
+			$query=" UPDATE t_sms_outbox
+		   		SET is_sent='Y',date_sent=sysdate
+		 		WHERE t_sms_outbox_id=".$data[$counter]['t_sms_outbox_id'];
+			$dbConn->query($query);
+		}
 	}else{
 		echo "no data";
 	}
@@ -43,52 +90,6 @@ function sendSms(){
     }
 	*/
     //$file_name = createExcel($no_telp,$npwd);
-	$file_name = createExcel($data);
-	date_default_timezone_set('Asia/Jakarta');
-	$send_date = date('Y-m-d-H-i-s');
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/_libz/usersignin.php");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    // in real life you should use something like:
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 
-                    array(  'loginUsername'  => 'disyanjak',//'radnettrial',
-                            'loginPass'    => 'disyanjakbdg12345678',//'trial',
-                            'attached_type' => 'action_1',
-                            'senderID'      => 'JMP000007',
-                            'sender'	    => 'DISYANJAK',
-                            'tanggal'	    => substr($send_date,0,10),//'2014-03-11',
-                            'jam1'	        => substr($send_date,11,2),//'16',
-                            'mnt1'	        => ''.(substr($send_date,14,2)+1).'',//'19',
-                            'pesan'	        => '',
-                            'tb_simpan'	    => 'Submit',
-                            'login_btn'     => 'Login',
-                            'nmbatch'       => '@' . realpath($file_name.'.xls') . ';filename='.$file_name.'_'.$send_date.'.xls'
-                          )
-                    );
-    
-    // receive server response ...
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
-    
-    $tmp_fname = tempnam("/tmp", "COOKIE");
-     
-    curl_setopt ($ch, CURLOPT_COOKIEJAR, $tmp_fname);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-    
-    $server_output = curl_exec ($ch);
-    
-    curl_setopt($ch, CURLOPT_URL,"http://smsblast.radbdg.net/userarea/p/personalize_exe.php");
-    curl_setopt ($ch, CURLOPT_COOKIEFILE, $tmp_fname);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-    $server_output = curl_exec ($ch);
-	echo $server_output;
-    curl_close ($ch);
-
-	for($counter = 0;$counter < sizeof($data);$counter++){
-		$query=" UPDATE t_sms_outbox
-	   		SET is_sent='Y',date_sent=sysdate
-	 		WHERE t_sms_outbox_id=".$data[$counter]['t_sms_outbox_id'];
-		$dbConn->query($query);
-	}
+	
 }
 sendSms();
