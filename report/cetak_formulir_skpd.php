93d2e54c60fd7cacf4614281920530facd467f03
fajar.rusius <fajar.rusius@gmail.com>
2014-09-04 03:28:32 +0000

    ''

--- report/cetak_formulir_skpd.php
+++ report/cetak_formulir_skpd.php
@@ -450,7 +450,7 @@ class FormCetak extends FPDF {
 		
 		$this->Image('http://'.$_SERVER['HTTP_HOST'].'/mpd/include/qrcode/generate-qr.php?param='.$data["npwd"]."-".str_replace(" ","-",$data["finance_period_code"]),15,190,25,25,'PNG');
 
-		/*
+		
 		$this->Cell($lbody3 - 10, $this->height, "Bandung, " . Date("d M Y"), "L", 0, 'C');
 		$this->Cell($lbody1 + 10, $this->height, "", "R", 0, 'C');
 		$this->Ln();
@@ -472,8 +472,8 @@ class FormCetak extends FPDF {
 		$this->Cell($lbody3 - 10, $this->height, "NIP. 19691104 199803 1 007", "BL", 0, 'C');
 		$this->Cell($lbody1 + 10, $this->height, "", "BR", 0, 'C');
 		$this->Ln($this->height + 4);
-		*/	
-		$this->Cell($this->lengthCell, $this->height, "", "LBR", 0, 'C');
+			
+		//$this->Cell($this->lengthCell, $this->height, "", "LBR", 0, 'C');
 		
 	}
 
