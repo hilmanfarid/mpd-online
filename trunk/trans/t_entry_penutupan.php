8a6ff329cade11a48888fe4a0e5a793e8a731659
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-10-09 10:50:38 +0000

    ''

---
+++ trunk/trans/t_entry_penutupan.php
@@ -0,0 +1,374 @@
+<?php
+//Include Common Files @1-F179400C
+define("RelativePath", "..");
+define("PathToCurrentPage", "/trans/");
+define("FileName", "t_entry_penutupan.php");
+include_once(RelativePath . "/Common.php");
+include_once(RelativePath . "/Template.php");
+include_once(RelativePath . "/Sorter.php");
+include_once(RelativePath . "/Navigator.php");
+//End Include Common Files
+
+class clsRecordt_vat_setllementForm { //t_vat_setllementForm Class @23-D94969C3
+
+//Variables @23-D6FF3E86
+
+    // Public variables
+    var $ComponentType = "Record";
+    var $ComponentName;
+    var $Parent;
+    var $HTMLFormAction;
+    var $PressedButton;
+    var $Errors;
+    var $ErrorBlock;
+    var $FormSubmitted;
+    var $FormEnctype;
+    var $Visible;
+    var $IsEmpty;
+
+    var $CCSEvents = "";
+    var $CCSEventResult;
+
+    var $RelativePath = "";
+
+    var $InsertAllowed = false;
+    var $UpdateAllowed = false;
+    var $DeleteAllowed = false;
+    var $ReadAllowed   = false;
+    var $EditMode      = false;
+    var $ds;
+    var $DataSource;
+    var $ValidatingControls;
+    var $Controls;
+    var $Attributes;
+
+    // Class variables
+//End Variables
+
+//Class_Initialize Event @23-F55CB155
+    function clsRecordt_vat_setllementForm($RelativePath, & $Parent)
+    {
+
+        global $FileName;
+        global $CCSLocales;
+        global $DefaultDateFormat;
+        $this->Visible = true;
+        $this->Parent = & $Parent;
+        $this->RelativePath = $RelativePath;
+        $this->Errors = new clsErrors();
+        $this->ErrorBlock = "Record t_vat_setllementForm/Error";
+        $this->ReadAllowed = true;
+        if($this->Visible)
+        {
+            $this->ComponentName = "t_vat_setllementForm";
+            $this->Attributes = new clsAttributes($this->ComponentName . ":");
+            $CCSForm = explode(":", CCGetFromGet("ccsForm", ""), 2);
+            if(sizeof($CCSForm) == 1)
+                $CCSForm[1] = "";
+            list($FormName, $FormMethod) = $CCSForm;
+            $this->FormEnctype = "application/x-www-form-urlencoded";
+            $this->FormSubmitted = ($FormName == $this->ComponentName);
+            $Method = $this->FormSubmitted ? ccsPost : ccsGet;
+            $this->creation_date = & new clsControl(ccsTextBox, "creation_date", "Creation Date", ccsText, "", CCGetRequestParam("creation_date", $Method, NULL), $this);
+            $this->created_by = & new clsControl(ccsTextBox, "created_by", "Created By", ccsText, "", CCGetRequestParam("created_by", $Method, NULL), $this);
+            $this->updated_date = & new clsControl(ccsTextBox, "updated_date", "Updated Date", ccsText, "", CCGetRequestParam("updated_date", $Method, NULL), $this);
+            $this->updated_by = & new clsControl(ccsTextBox, "updated_by", "Updated By", ccsText, "", CCGetRequestParam("updated_by", $Method, NULL), $this);
+            $this->npwd = & new clsControl(ccsTextBox, "npwd", "NPWD", ccsText, "", CCGetRequestParam("npwd", $Method, NULL), $this);
+            $this->npwd->Required = true;
+            $this->t_cust_account_id = & new clsControl(ccsHidden, "t_cust_account_id", "t_cust_account_id", ccsFloat, "", CCGetRequestParam("t_cust_account_id", $Method, NULL), $this);
+            $this->p_account_status_mut = & new clsControl(ccsTextBox, "p_account_status_mut", "Periode", ccsText, "", CCGetRequestParam("p_account_status_mut", $Method, NULL), $this);
+            $this->p_account_status_mut->Required = true;
+            $this->p_account_status_id = & new clsControl(ccsHidden, "p_account_status_id", "p_account_status_id", ccsFloat, "", CCGetRequestParam("p_account_status_id", $Method, NULL), $this);
+            $this->Button1 = & new clsButton("Button1", $Method, $this);
+            $this->company_name = & new clsControl(ccsTextBox, "company_name", "Nama Perusahaan", ccsText, "", CCGetRequestParam("company_name", $Method, NULL), $this);
+            $this->reason_code = & new clsControl(ccsTextBox, "reason_code", "Ayat Pajak", ccsText, "", CCGetRequestParam("reason_code", $Method, NULL), $this);
+            $this->reason_code->Required = true;
+            $this->reason_status_id = & new clsControl(ccsHidden, "reason_status_id", "reason_status_id", ccsText, "", CCGetRequestParam("reason_status_id", $Method, NULL), $this);
+            $this->reason_description = & new clsControl(ccsTextBox, "reason_description", "keterangan alasan penutupan", ccsText, "", CCGetRequestParam("reason_description", $Method, NULL), $this);
+            if(!$this->FormSubmitted) {
+                if(!is_array($this->creation_date->Value) && !strlen($this->creation_date->Value) && $this->creation_date->Value !== false)
+                    $this->creation_date->SetText(date("d-M-Y"));
+                if(!is_array($this->created_by->Value) && !strlen($this->created_by->Value) && $this->created_by->Value !== false)
+                    $this->created_by->SetText(CCGetUserLogin());
+                if(!is_array($this->updated_date->Value) && !strlen($this->updated_date->Value) && $this->updated_date->Value !== false)
+                    $this->updated_date->SetText(date("d-M-Y"));
+                if(!is_array($this->updated_by->Value) && !strlen($this->updated_by->Value) && $this->updated_by->Value !== false)
+                    $this->updated_by->SetText(CCGetUserLogin());
+            }
+        }
+    }
+//End Class_Initialize Event
+
+//Validate Method @23-E8EFC676
+    function Validate()
+    {
+        global $CCSLocales;
+        $Validation = true;
+        $Where = "";
+        $Validation = ($this->creation_date->Validate() && $Validation);
+        $Validation = ($this->created_by->Validate() && $Validation);
+        $Validation = ($this->updated_date->Validate() && $Validation);
+        $Validation = ($this->updated_by->Validate() && $Validation);
+        $Validation = ($this->npwd->Validate() && $Validation);
+        $Validation = ($this->t_cust_account_id->Validate() && $Validation);
+        $Validation = ($this->p_account_status_mut->Validate() && $Validation);
+        $Validation = ($this->p_account_status_id->Validate() && $Validation);
+        $Validation = ($this->company_name->Validate() && $Validation);
+        $Validation = ($this->reason_code->Validate() && $Validation);
+        $Validation = ($this->reason_status_id->Validate() && $Validation);
+        $Validation = ($this->reason_description->Validate() && $Validation);
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "OnValidate", $this);
+        $Validation =  $Validation && ($this->creation_date->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->created_by->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->updated_date->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->updated_by->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->npwd->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->t_cust_account_id->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->p_account_status_mut->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->p_account_status_id->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->company_name->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->reason_code->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->reason_status_id->Errors->Count() == 0);
+        $Validation =  $Validation && ($this->reason_description->Errors->Count() == 0);
+        return (($this->Errors->Count() == 0) && $Validation);
+    }
+//End Validate Method
+
+//CheckErrors Method @23-A584C40C
+    function CheckErrors()
+    {
+        $errors = false;
+        $errors = ($errors || $this->creation_date->Errors->Count());
+        $errors = ($errors || $this->created_by->Errors->Count());
+        $errors = ($errors || $this->updated_date->Errors->Count());
+        $errors = ($errors || $this->updated_by->Errors->Count());
+        $errors = ($errors || $this->npwd->Errors->Count());
+        $errors = ($errors || $this->t_cust_account_id->Errors->Count());
+        $errors = ($errors || $this->p_account_status_mut->Errors->Count());
+        $errors = ($errors || $this->p_account_status_id->Errors->Count());
+        $errors = ($errors || $this->company_name->Errors->Count());
+        $errors = ($errors || $this->reason_code->Errors->Count());
+        $errors = ($errors || $this->reason_status_id->Errors->Count());
+        $errors = ($errors || $this->reason_description->Errors->Count());
+        $errors = ($errors || $this->Errors->Count());
+        return $errors;
+    }
+//End CheckErrors Method
+
+//MasterDetail @23-ED598703
+function SetPrimaryKeys($keyArray)
+{
+    $this->PrimaryKeys = $keyArray;
+}
+function GetPrimaryKeys()
+{
+    return $this->PrimaryKeys;
+}
+function GetPrimaryKey($keyName)
+{
+    return $this->PrimaryKeys[$keyName];
+}
+//End MasterDetail
+
+//Operation Method @23-DE5BDEFB
+    function Operation()
+    {
+        if(!$this->Visible)
+            return;
+
+        global $Redirect;
+        global $FileName;
+
+        if(!$this->FormSubmitted) {
+            return;
+        }
+
+        if($this->FormSubmitted) {
+            $this->PressedButton = "Button1";
+            if($this->Button1->Pressed) {
+                $this->PressedButton = "Button1";
+            }
+        }
+        $Redirect = $FileName . "?" . CCGetQueryString("QueryString", array("ccsForm"));
+        if($this->Validate()) {
+            if($this->PressedButton == "Button1") {
+                if(!CCGetEvent($this->Button1->CCSEvents, "OnClick", $this->Button1)) {
+                    $Redirect = "";
+                }
+            }
+        } else {
+            $Redirect = "";
+        }
+    }
+//End Operation Method
+
+//Show Method @23-F5B0F309
+    function Show()
+    {
+        global $CCSUseAmp;
+        global $Tpl;
+        global $FileName;
+        global $CCSLocales;
+        $Error = "";
+
+        if(!$this->Visible)
+            return;
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeSelect", $this);
+
+
+        $RecordBlock = "Record " . $this->ComponentName;
+        $ParentPath = $Tpl->block_path;
+        $Tpl->block_path = $ParentPath . "/" . $RecordBlock;
+        $this->EditMode = $this->EditMode && $this->ReadAllowed;
+        if (!$this->FormSubmitted) {
+        }
+
+        if($this->FormSubmitted || $this->CheckErrors()) {
+            $Error = "";
+            $Error = ComposeStrings($Error, $this->creation_date->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->created_by->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->updated_date->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->updated_by->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->npwd->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->t_cust_account_id->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->p_account_status_mut->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->p_account_status_id->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->company_name->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->reason_code->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->reason_status_id->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->reason_description->Errors->ToString());
+            $Error = ComposeStrings($Error, $this->Errors->ToString());
+            $Tpl->SetVar("Error", $Error);
+            $Tpl->Parse("Error", false);
+        }
+        $CCSForm = $this->EditMode ? $this->ComponentName . ":" . "Edit" : $this->ComponentName;
+        $this->HTMLFormAction = $FileName . "?" . CCAddParam(CCGetQueryString("QueryString", ""), "ccsForm", $CCSForm);
+        $Tpl->SetVar("Action", !$CCSUseAmp ? $this->HTMLFormAction : str_replace("&", "&amp;", $this->HTMLFormAction));
+        $Tpl->SetVar("HTMLFormName", $this->ComponentName);
+        $Tpl->SetVar("HTMLFormEnctype", $this->FormEnctype);
+
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeShow", $this);
+        $this->Attributes->Show();
+        if(!$this->Visible) {
+            $Tpl->block_path = $ParentPath;
+            return;
+        }
+
+        $this->creation_date->Show();
+        $this->created_by->Show();
+        $this->updated_date->Show();
+        $this->updated_by->Show();
+        $this->npwd->Show();
+        $this->t_cust_account_id->Show();
+        $this->p_account_status_mut->Show();
+        $this->p_account_status_id->Show();
+        $this->Button1->Show();
+        $this->company_name->Show();
+        $this->reason_code->Show();
+        $this->reason_status_id->Show();
+        $this->reason_description->Show();
+        $Tpl->parse();
+        $Tpl->block_path = $ParentPath;
+    }
+//End Show Method
+
+} //End t_vat_setllementForm Class @23-FCB6E20C
+
+//Initialize Page @1-DF6E7E5A
+// Variables
+$FileName = "";
+$Redirect = "";
+$Tpl = "";
+$TemplateFileName = "";
+$BlockToParse = "";
+$ComponentName = "";
+$Attributes = "";
+
+// Events;
+$CCSEvents = "";
+$CCSEventResult = "";
+
+$FileName = FileName;
+$Redirect = "";
+$TemplateFileName = "t_entry_penutupan.html";
+$BlockToParse = "main";
+$TemplateEncoding = "CP1252";
+$ContentType = "text/html";
+$PathToRoot = "../";
+$Charset = $Charset ? $Charset : "windows-1252";
+//End Initialize Page
+
+//Include events file @1-70EBA8A7
+include_once("./t_entry_penutupan_events.php");
+//End Include events file
+
+//Before Initialize @1-E870CEBC
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeInitialize", $MainPage);
+//End Before Initialize
+
+//Initialize Objects @1-E84E3EAB
+$DBConnSIKP = new clsDBConnSIKP();
+$MainPage->Connections["ConnSIKP"] = & $DBConnSIKP;
+$Attributes = new clsAttributes("page:");
+$MainPage->Attributes = & $Attributes;
+
+// Controls
+$t_vat_setllementForm = & new clsRecordt_vat_setllementForm("", $MainPage);
+$MainPage->t_vat_setllementForm = & $t_vat_setllementForm;
+
+BindEvents();
+
+$CCSEventResult = CCGetEvent($CCSEvents, "AfterInitialize", $MainPage);
+
+if ($Charset) {
+    header("Content-Type: " . $ContentType . "; charset=" . $Charset);
+} else {
+    header("Content-Type: " . $ContentType);
+}
+//End Initialize Objects
+
+//Initialize HTML Template @1-52F9C312
+$CCSEventResult = CCGetEvent($CCSEvents, "OnInitializeView", $MainPage);
+$Tpl = new clsTemplate($FileEncoding, $TemplateEncoding);
+$Tpl->LoadTemplate(PathToCurrentPage . $TemplateFileName, $BlockToParse, "CP1252");
+$Tpl->block_path = "/$BlockToParse";
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeShow", $MainPage);
+$Attributes->SetValue("pathToRoot", "../");
+$Attributes->Show();
+//End Initialize HTML Template
+
+//Execute Components @1-066D187A
+$t_vat_setllementForm->Operation();
+//End Execute Components
+
+//Go to destination page @1-41353ACE
+if($Redirect)
+{
+    $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+    $DBConnSIKP->close();
+    header("Location: " . $Redirect);
+    unset($t_vat_setllementForm);
+    unset($Tpl);
+    exit;
+}
+//End Go to destination page
+
+//Show Page @1-5746EF7A
+$t_vat_setllementForm->Show();
+$Tpl->block_path = "";
+$Tpl->Parse($BlockToParse, false);
+if (!isset($main_block)) $main_block = $Tpl->GetVar($BlockToParse);
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeOutput", $MainPage);
+if ($CCSEventResult) echo $main_block;
+//End Show Page
+
+//Unload Page @1-825C45A1
+$CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
+$DBConnSIKP->close();
+unset($t_vat_setllementForm);
+unset($Tpl);
+//End Unload Page
+
+
+?>
