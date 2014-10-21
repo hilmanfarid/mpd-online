4452ae5ed1e13dee69d5ac3f97aeceb8e30f7e46
wiliamdecosta <wiliamdecosta@gmail.com>
2014-10-21 03:36:18 +0000

    ''

--- param/t_customer_report_events.php
+++ param/t_customer_report_events.php
@@ -2,7 +2,7 @@
 $add_flag=CCGetFromGet("FLAG", "NONE");
 $is_show_form=($add_flag=="ADD");
 
-//BindEvents Method @1-EC23321B
+//BindEvents Method @1-F5F17B71
 function BindEvents()
 {
     global $t_customerGrid;
@@ -12,6 +12,7 @@ function BindEvents()
     $t_customerGrid->CCSEvents["BeforeSelect"] = "t_customerGrid_BeforeSelect";
     $t_customerSearch->CCSEvents["BeforeShow"] = "t_customerSearch_BeforeShow";
     $CCSEvents["OnInitializeView"] = "Page_OnInitializeView";
+    $CCSEvents["BeforeShow"] = "Page_BeforeShow";
 }
 //End BindEvents Method
 
@@ -153,4 +154,97 @@ function Page_OnInitializeView(& $sender)
 }
 //End Close Page_OnInitializeView
 
+//Page_BeforeShow @1-3DE84F02
+function Page_BeforeShow(& $sender)
+{
+    $Page_BeforeShow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_customer_report; //Compatibility
+//End Page_BeforeShow
+
+//Custom Code @538-2A29BDB7
+// -------------------------
+    if(CCGetFromGet('view_report') == 'cetak_excel') {
+		$param_arr=array();
+		$param_arr['s_npwd'] = CCGetFromGet('s_npwd');
+		$param_arr['p_vat_type_id'] = CCGetFromGet('p_vat_type_id');
+		$param_arr['p_vat_type_dtl_id'] = CCGetFromGet('p_vat_type_dtl_id');
+		$param_arr['s_wp_name'] = CCGetFromGet('s_wp_name');
+		$param_arr['s_company_name'] = CCGetFromGet('s_company_name');
+		$param_arr['s_company_brand'] = CCGetFromGet('s_company_brand');
+		$param_arr['s_keyword'] = CCGetFromGet('s_keyword');
+		print_excel($param_arr);
+	}
+// -------------------------
+//End Custom Code
+
+//Close Page_BeforeShow @1-4BC230CD
+    return $Page_BeforeShow;
+}
+//End Close Page_BeforeShow
+function startExcel($filename = "laporan.xls") {
+    
+	header("Content-type: application/vnd.ms-excel");
+	header("Content-Disposition: attachment; filename=$filename");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+	header("Pragma: public");
+}
+
+function print_excel($param_arr) {
+
+	startExcel("daftar_wajib_pajak".date('Y-m-d'));
+	echo '<div align="center"><h3> DAFTAR WAJIB PAJAK</h3></div>';	
+		
+	echo '<table border="1">';
+	echo '<tr>
+		<th>NO</th>
+		<th>NAMA PEMILIK/PENGELOLA</th>
+		<th>NPWD</th>
+		<th>ALAMAT</th>
+		<th>JENIS PAJAK</th>
+		<th>AYAT PAJAK</th>
+		<th>NO SELULAR</th>
+		<th>EMAIL</th>
+	</tr>';
+	
+	
+	$dbConn = new clsDBConnSIKP();
+	
+	$query="select a.*, b.npwd, c.vat_code, d.vat_code as detail_pajak_code
+			FROM t_customer a
+			LEFT JOIN t_cust_account b ON a.t_customer_id = b.t_customer_id
+			LEFT JOIN p_vat_type c ON b.p_vat_type_id = c.p_vat_type_id
+			LEFT JOIN p_vat_type_dtl d ON b.p_vat_type_dtl_id = d.p_vat_type_dtl_id
+
+			WHERE upper(a.company_owner) like upper('%".$param_arr['s_keyword']."%') 
+			       and upper(a.address_name_owner) like upper('%".$param_arr['s_keyword']."%')
+			       and upper(b.npwd) like upper('%".$param_arr['s_npwd']."%')
+			       and upper(b.wp_name) like upper('%".$param_arr['s_wp_name']."%')
+			       and upper(b.company_name) like upper('%".$param_arr['s_company_name']."%')
+			       and upper(b.company_brand) like upper('%".$param_arr['s_company_brand']."%')
+				   and b.p_vat_type_id like '%".$param_arr['p_vat_type_id']."%'
+				   and b.p_vat_type_dtl_id like '%".$param_arr['p_vat_type_dtl_id']."%'
+				   and b.p_account_status_id = 1
+				   ";
+	$dbConn->query($query);
+	
+	$no = 1;
+	while($dbConn->next_record()){
+		echo '<tr>';
+		echo '<td valign="top" align="center">'.$no++.'</td>';
+		echo '<td valign="top" >'.$dbConn->f("company_owner").'</td>';
+		echo '<td valign="top" align="center">'.$dbConn->f("npwd").'</td>';
+		echo '<td valign="top" >'.$dbConn->f("address_name_owner")." No ".$dbConn->f("address_no_owner")." RT/RW : ".$dbConn->f("address_rt_owner")."/".$dbConn->f("address_rw_owner").'</td>';
+		echo '<td valign="top" >'.$dbConn->f("vat_code").'</td>';
+		echo '<td valign="top" >'.$dbConn->f("detail_pajak_code").'</td>';
+		echo '<td valign="top" >'.$dbConn->f("mobile_no_owner").'</td>';
+		echo '<td valign="top" >'.$dbConn->f("email_address").'</td>';
+		echo '</tr>';
+	}
+
+	echo '</table>';
+	exit;
+}
 ?>
