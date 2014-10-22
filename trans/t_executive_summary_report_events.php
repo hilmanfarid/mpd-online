10226b6088e51acf8b5b6bb842a632f9a83514dc
haxbox.keren <haxbox.keren@gmail.com>
2014-10-22 09:50:14 +0000

    ''

--- trans/t_executive_summary_report_events.php
+++ trans/t_executive_summary_report_events.php
@@ -1,10 +1,12 @@
 <?php
-//BindEvents Method @1-1D7E4284
+//BindEvents Method @1-D729CD9D
 function BindEvents()
 {
     global $t_executive_summary_form;
     global $CCSEvents;
     $t_executive_summary_form->CCSEvents["BeforeSelect"] = "t_executive_summary_form_BeforeSelect";
+    $t_executive_summary_form->CCSEvents["AfterInsert"] = "t_executive_summary_form_AfterInsert";
+    $t_executive_summary_form->CCSEvents["BeforeInsert"] = "t_executive_summary_form_BeforeInsert";
     $CCSEvents["BeforeShow"] = "Page_BeforeShow";
 }
 //End BindEvents Method
@@ -28,6 +30,53 @@ function t_executive_summary_form_BeforeSelect(& $sender)
     return $t_executive_summary_form_BeforeSelect;
 }
 //End Close t_executive_summary_form_BeforeSelect
+
+//t_executive_summary_form_AfterInsert @25-FDD789E2
+function t_executive_summary_form_AfterInsert(& $sender)
+{
+    $t_executive_summary_form_AfterInsert = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_executive_summary_form; //Compatibility
+//End t_executive_summary_form_AfterInsert
+
+//Custom Code @132-2A29BDB7
+// -------------------------
+    // Write your own code here.
+	$t_executive_summary_form->DataSource->next_record();
+	$records = $t_executive_summary_form->DataSource->Record;
+	if($t_executive_summary_form->DataSource->Errors['ErrorsCount']==0){
+		$dbConn = new clsDBConnSIKP();
+		$sql="select sikp.f_first_submit_engine(508,".$records['o_t_customer_order_id'].",'".CCGetSession('UserLogin')."')";
+		$dbConn->query($sql);
+	}
+// -------------------------
+//End Custom Code
+
+//Close t_executive_summary_form_AfterInsert @25-96DE34DB
+    return $t_executive_summary_form_AfterInsert;
+}
+//End Close t_executive_summary_form_AfterInsert
+
+//t_executive_summary_form_BeforeInsert @25-2B2157E6
+function t_executive_summary_form_BeforeInsert(& $sender)
+{
+    $t_executive_summary_form_BeforeInsert = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_executive_summary_form; //Compatibility
+//End t_executive_summary_form_BeforeInsert
+
+//Custom Code @133-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close t_executive_summary_form_BeforeInsert @25-40B865D6
+    return $t_executive_summary_form_BeforeInsert;
+}
+//End Close t_executive_summary_form_BeforeInsert
 
 //Page_BeforeShow @1-6B6917AB
 function Page_BeforeShow(& $sender)
