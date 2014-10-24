e42ce049fe634b4cee861d9aa2ac9983f568822d
fajar.rusius <fajar.rusius@gmail.com>
2014-10-24 09:09:31 +0000

    ''

--- trans/t_executive_summary_report_read_only_events.php
+++ trans/t_executive_summary_report_read_only_events.php
@@ -43,19 +43,67 @@ function Page_BeforeShow(& $sender)
     // Write your own code here.
 	global $t_executive_sumary_filter;
 	global $t_executive_summary_form;
-	$periode = $t_executive_sumary_filter->p_finance_period_id->GetValue();
-	$pajak = $t_executive_sumary_filter->p_vat_type_id->GetValue();
-	$period_type = $t_executive_sumary_filter->ListBox1->GetValue();
+	$view = CCGetFromGet("lihat_laporan", "F");
+	if ($view=="T"){
+		$periode = CCGetFromGet("p_year_period_id", "F");
+		$pajak = CCGetFromGet("p_vat_type_id", "F");
+		$period_type = CCGetFromGet("ListBox1", "1");
 
-	if (
-		empty($pajak)
-		&&
-		empty($periode)
-		&&
-		empty($period_type)
-	){
+		if ($period_type==1)
+		{
+			$p_finance_period_id = CCGetFromGet("p_finance_period_id", "1");
+			$dbConn = new clsDBConnSIKP();
+			$query = "select * from t_executive_summary where period_type=1 
+				and p_finance_period_id=".$p_finance_period_id;
+			$dbConn->query($query);
+			while ($dbConn->next_record()) {
+				$t_executive_summary_form->penjelasan->SetValue( $dbConn->f("penjelasan") );
+				$t_executive_summary_form->permasalahan->SetValue( $dbConn->f("permasalahan") );
+				$t_executive_summary_form->saran->SetValue( $dbConn->f("saran") );
+				$t_executive_summary_form->kesimpulan->SetValue( $dbConn->f("kesimpulan") );
+			}
+			$dbConn->close();
+		}
+		if ($period_type==2)
+		{	
+			$ListBox2 = CCGetFromGet("ListBox2", "1");
+			$dbConn = new clsDBConnSIKP();
+			$query = "select * from t_executive_summary where period_type=2 
+				and triwulan=".$ListBox2;
+			$dbConn->query($query);
+			while ($dbConn->next_record()) {
+				$t_executive_summary_form->penjelasan->SetValue( $dbConn->f("penjelasan") );
+				$t_executive_summary_form->permasalahan->SetValue( $dbConn->f("permasalahan") );
+				$t_executive_summary_form->saran->SetValue( $dbConn->f("saran") );
+				$t_executive_summary_form->kesimpulan->SetValue( $dbConn->f("kesimpulan") );
+			}
+			$dbConn->close();
+		}
+		if ($period_type==3)
+		{
+			$ListBox3 = CCGetFromGet("ListBox3", "1");
+			$dbConn = new clsDBConnSIKP();
+			$query = "select * from t_executive_summary where period_type=3 
+			and semester=".$ListBox3;
+			$dbConn->query($query);
+			while ($dbConn->next_record()) {
+				$t_executive_summary_form->penjelasan->SetValue( $dbConn->f("penjelasan") );
+				$t_executive_summary_form->permasalahan->SetValue( $dbConn->f("permasalahan") );
+				$t_executive_summary_form->saran->SetValue( $dbConn->f("saran") );
+				$t_executive_summary_form->kesimpulan->SetValue( $dbConn->f("kesimpulan") );
+			}
+			$dbConn->close();
+		}
 		
-	}else{
+	}
+	
+	
+	/*
+	if (empty($period_type))
+	{
+	
+	}else{	
+
 		if ($t_executive_sumary_filter->ListBox1->GetValue()==1)
 		{
 			$dbConn = new clsDBConnSIKP();
@@ -96,6 +144,7 @@ function Page_BeforeShow(& $sender)
 			$dbConn->close();
 		}
 	}
+	*/
 
 // -------------------------
 //End Custom Code
