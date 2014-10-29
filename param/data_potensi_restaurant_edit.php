f927110d280727b05e0c291071eb27edca932b19
fajar.rusius <fajar.rusius@gmail.com>
2014-10-29 05:41:51 +0000

    ''

--- param/data_potensi_restaurant_edit.php
+++ param/data_potensi_restaurant_edit.php
@@ -45,7 +45,7 @@ class clsRecordt_vat_reg_dtl_restaurantForm { //t_vat_reg_dtl_restaurantForm Cla
     // Class variables
 //End Variables
 
-//Class_Initialize Event @94-D1DAF700
+//Class_Initialize Event @94-847C4A1C
     function clsRecordt_vat_reg_dtl_restaurantForm($RelativePath, & $Parent)
     {
 
@@ -61,6 +61,7 @@ class clsRecordt_vat_reg_dtl_restaurantForm { //t_vat_reg_dtl_restaurantForm Cla
         $this->ds = & $this->DataSource;
         $this->InsertAllowed = true;
         $this->UpdateAllowed = true;
+        $this->DeleteAllowed = true;
         $this->ReadAllowed = true;
         if($this->Visible)
         {
@@ -219,7 +220,7 @@ function GetPrimaryKey($keyName)
 }
 //End MasterDetail
 
-//Operation Method @94-AAE4F942
+//Operation Method @94-7DE29AD7
     function Operation()
     {
         if(!$this->Visible)
@@ -249,7 +250,7 @@ function GetPrimaryKey($keyName)
         $Redirect = $FileName . "?" . CCGetQueryString("QueryString", array("ccsForm"));
         if($this->PressedButton == "Button_Delete") {
             $Redirect = $FileName . "?" . CCGetQueryString("QueryString", array("ccsForm", "FLAG", "t_vat_reg_dtl_hotel_id"));
-            if(!CCGetEvent($this->Button_Delete->CCSEvents, "OnClick", $this->Button_Delete)) {
+            if(!CCGetEvent($this->Button_Delete->CCSEvents, "OnClick", $this->Button_Delete) || !$this->DeleteRow()) {
                 $Redirect = "";
             }
         } else if($this->PressedButton == "Button_Cancel") {
@@ -316,6 +317,18 @@ function GetPrimaryKey($keyName)
         return (!$this->CheckErrors());
     }
 //End UpdateRow Method
+
+//DeleteRow Method @94-2CC9E74B
+    function DeleteRow()
+    {
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeDelete", $this);
+        if(!$this->DeleteAllowed) return false;
+        $this->DataSource->t_cacc_dtl_restaurant_id->SetValue($this->t_cacc_dtl_restaurant_id->GetValue(true));
+        $this->DataSource->Delete();
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "AfterDelete", $this);
+        return (!$this->CheckErrors());
+    }
+//End DeleteRow Method
 
 //Show Method @94-1D76859B
     function Show()
@@ -443,7 +456,7 @@ function GetPrimaryKey($keyName)
 
 class clst_vat_reg_dtl_restaurantFormDataSource extends clsDBConnSIKP {  //t_vat_reg_dtl_restaurantFormDataSource Class @94-55268935
 
-//DataSource Variables @94-19DDE1FC
+//DataSource Variables @94-3F381887
     var $Parent = "";
     var $CCSEvents = "";
     var $CCSEventResult;
@@ -452,6 +465,7 @@ class clst_vat_reg_dtl_restaurantFormDataSource extends clsDBConnSIKP {  //t_vat
 
     var $InsertParameters;
     var $UpdateParameters;
+    var $DeleteParameters;
     var $wp;
     var $AllParametersSet;
 
@@ -683,6 +697,28 @@ class clst_vat_reg_dtl_restaurantFormDataSource extends clsDBConnSIKP {  //t_vat
         }
     }
 //End Update Method
+
+//Delete Method @94-B0647BC8
+    function Delete()
+    {
+        global $CCSLocales;
+        global $DefaultDateFormat;
+        $this->CmdExecution = true;
+        $this->cp["t_cacc_dtl_restaurant_id"] = new clsSQLParameter("ctrlt_cacc_dtl_restaurant_id", ccsFloat, "", "", $this->t_cacc_dtl_restaurant_id->GetValue(true), 0, false, $this->ErrorBlock);
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeBuildDelete", $this->Parent);
+        if (!is_null($this->cp["t_cacc_dtl_restaurant_id"]->GetValue()) and !strlen($this->cp["t_cacc_dtl_restaurant_id"]->GetText()) and !is_bool($this->cp["t_cacc_dtl_restaurant_id"]->GetValue())) 
+            $this->cp["t_cacc_dtl_restaurant_id"]->SetValue($this->t_cacc_dtl_restaurant_id->GetValue(true));
+        if (!strlen($this->cp["t_cacc_dtl_restaurant_id"]->GetText()) and !is_bool($this->cp["t_cacc_dtl_restaurant_id"]->GetValue(true))) 
+            $this->cp["t_cacc_dtl_restaurant_id"]->SetText(0);
+        $this->SQL = "DELETE FROM t_cacc_dtl_restaurant\n" .
+        "WHERE t_cacc_dtl_restaurant_id = " . $this->SQLValue($this->cp["t_cacc_dtl_restaurant_id"]->GetDBValue(), ccsFloat) . "";
+        $this->CCSEventResult = CCGetEvent($this->CCSEvents, "BeforeExecuteDelete", $this->Parent);
+        if($this->Errors->Count() == 0 && $this->CmdExecution) {
+            $this->query($this->SQL);
+            $this->CCSEventResult = CCGetEvent($this->CCSEvents, "AfterExecuteDelete", $this->Parent);
+        }
+    }
+//End Delete Method
 
 } //End t_vat_reg_dtl_restaurantFormDataSource Class @94-FCB6E20C
 
