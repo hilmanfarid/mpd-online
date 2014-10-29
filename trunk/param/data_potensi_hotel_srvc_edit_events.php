4183aae549d894f9531fba80d202ce61d17f6f73
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-10-29 05:40:44 +0000

    ''

--- trunk/param/data_potensi_hotel_srvc_edit_events.php
+++ trunk/param/data_potensi_hotel_srvc_edit_events.php
@@ -2,13 +2,14 @@
 $add_flag=CCGetFromGet("FLAG", "NONE");
 $is_show_form=($add_flag=="ADD");
 
-//BindEvents Method @1-6E73D76B
+//BindEvents Method @1-4AFA8EA9
 function BindEvents()
 {
     global $t_vat_reg_dtl_hotel_srvcForm;
     global $CCSEvents;
     $t_vat_reg_dtl_hotel_srvcForm->ds->CCSEvents["AfterExecuteUpdate"] = "t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteUpdate";
     $t_vat_reg_dtl_hotel_srvcForm->ds->CCSEvents["AfterExecuteInsert"] = "t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteInsert";
+    $t_vat_reg_dtl_hotel_srvcForm->ds->CCSEvents["AfterExecuteDelete"] = "t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete";
     $CCSEvents["OnInitializeView"] = "Page_OnInitializeView";
 }
 //End BindEvents Method
@@ -70,6 +71,33 @@ function t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteInsert(& $sender)
     return $t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteInsert;
 }
 //End Close t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteInsert
+
+//t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete @94-4AC111CA
+function t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete(& $sender)
+{
+    $t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_reg_dtl_hotel_srvcForm; //Compatibility
+//End t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete
+
+//Custom Code @883-2A29BDB7
+// -------------------------
+    // Write your own code here.
+	$id_vat = $t_vat_reg_dtl_hotel_srvcForm->t_cust_account_id->GetValue();
+	$redirectloader = "data_potensi_update.php?t_cust_account_id=".$id_vat;
+	echo ("<script language='javascript'>");
+    echo (" parent.window.opener.location.href = '" . $redirectloader . "';");
+	echo (" window.close(); ");
+	echo ("</script>");
+	exit;
+// -------------------------
+//End Custom Code
+
+//Close t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete @94-0587A8CC
+    return $t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete;
+}
+//End Close t_vat_reg_dtl_hotel_srvcForm_ds_AfterExecuteDelete
 
 //Page_OnInitializeView @1-D0E4DC6C
 function Page_OnInitializeView(& $sender)
