3410935d9315c6f1d05902e040a04eb4fb04f816
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-11-19 03:29:08 +0000

    ''

---
+++ trunk/trans/t_vat_setllement_cetak_skpd_penelitian_events.php
@@ -0,0 +1,153 @@
+<?php
+//include_once(RelativePath . "/include/sessi.inc");
+$add_flag=CCGetFromGet("FLAG", "NONE");
+$is_show_form=($add_flag=="ADD");
+
+//BindEvents Method @1-FDFEAE1D
+function BindEvents()
+{
+    global $t_vat_setllementGrid;
+    global $CCSEvents;
+    $t_vat_setllementGrid->CCSEvents["BeforeShowRow"] = "t_vat_setllementGrid_BeforeShowRow";
+    $t_vat_setllementGrid->CCSEvents["BeforeSelect"] = "t_vat_setllementGrid_BeforeSelect";
+    $t_vat_setllementGrid->CCSEvents["BeforeShow"] = "t_vat_setllementGrid_BeforeShow";
+    $CCSEvents["OnInitializeView"] = "Page_OnInitializeView";
+    $CCSEvents["BeforeShow"] = "Page_BeforeShow";
+}
+//End BindEvents Method
+
+//t_vat_setllementGrid_BeforeShowRow @2-292D3A2A
+function t_vat_setllementGrid_BeforeShowRow(& $sender)
+{
+    $t_vat_setllementGrid_BeforeShowRow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_setllementGrid; //Compatibility
+//End t_vat_setllementGrid_BeforeShowRow
+
+// Start Bdr
+    global $selected_id;
+    global $add_flag;
+    global $is_show_form;
+
+    if ($selected_id<0 && $add_flag!="ADD") {
+    	$selected_id = $Component->DataSource->t_vat_setllement_id->GetValue();
+   }
+// End Bdr  
+
+//Set Row Style @10-982C9472
+    $styles = array("Row", "AltRow");
+	// Start Bdr    
+      $img_radio= "<img border=\"0\" src=\"../images/radio.gif\">";
+      $Style = $styles[0];
+      
+      if ($Component->DataSource->t_vat_setllement_id->GetValue()== $selected_id) {
+      	$img_radio= "<img border=\"0\" src=\"../images/radio_s.gif\">";
+          $Style = $styles[1];
+          $is_show_form=1;
+      }	
+  // End Bdr  
+    if (count($styles)) {
+        //$Style = $styles[($Component->RowNumber - 1) % count($styles)];
+        if (strlen($Style) && !strpos($Style, "="))
+            $Style = (strpos($Style, ":") ? 'style="' : 'class="'). $Style . '"';
+        $Component->Attributes->SetValue("rowStyle", $Style);
+    }
+//End Set Row Style
+	 $Component->DLink->SetValue($img_radio); // Bdr
+
+//Close t_vat_setllementGrid_BeforeShowRow @2-CAEE8B40
+    return $t_vat_setllementGrid_BeforeShowRow;
+}
+//End Close t_vat_setllementGrid_BeforeShowRow
+
+//t_vat_setllementGrid_BeforeSelect @2-6B06F902
+function t_vat_setllementGrid_BeforeSelect(& $sender)
+{
+    $t_vat_setllementGrid_BeforeSelect = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_setllementGrid; //Compatibility
+//End t_vat_setllementGrid_BeforeSelect
+
+//Custom Code @124-2A29BDB7
+// -------------------------
+    // Write your own code here.
+	$Component->DataSource->Parameters["urls_keyword"] = strtoupper(CCGetFromGet("s_keyword", NULL));
+// -------------------------
+//End Custom Code
+
+//Close t_vat_setllementGrid_BeforeSelect @2-3DD75ADF
+    return $t_vat_setllementGrid_BeforeSelect;
+}
+//End Close t_vat_setllementGrid_BeforeSelect
+
+//t_vat_setllementGrid_BeforeShow @2-27F9F7A4
+function t_vat_setllementGrid_BeforeShow(& $sender)
+{
+    $t_vat_setllementGrid_BeforeShow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_setllementGrid; //Compatibility
+//End t_vat_setllementGrid_BeforeShow
+
+//Custom Code @316-2A29BDB7
+// -------------------------
+    if(CCGetFromGet("s_keyword") == "") {
+		$t_vat_setllementGrid->Visible = false;
+	}else {
+		$t_vat_setllementGrid->Visible = true;
+	}
+// -------------------------
+//End Custom Code
+
+//Close t_vat_setllementGrid_BeforeShow @2-542B3DA4
+    return $t_vat_setllementGrid_BeforeShow;
+}
+//End Close t_vat_setllementGrid_BeforeShow
+
+
+//Page_OnInitializeView @1-B8B501F0
+function Page_OnInitializeView(& $sender)
+{
+    $Page_OnInitializeView = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_setllement_cetak_skpd_penelitian; //Compatibility
+//End Page_OnInitializeView
+
+//Custom Code @66-2A29BDB7
+// -------------------------
+    // Write your own code here.
+	  global $selected_id;
+      $selected_id = -1;
+      $selected_id=CCGetFromGet("t_vat_setllement_id", $selected_id);
+// -------------------------
+//End Custom Code
+
+//Close Page_OnInitializeView @1-81DF8332
+    return $Page_OnInitializeView;
+}
+//End Close Page_OnInitializeView
+
+//Page_BeforeShow @1-F5A1ACD0
+function Page_BeforeShow(& $sender)
+{
+    $Page_BeforeShow = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_vat_setllement_cetak_skpd_penelitian; //Compatibility
+//End Page_BeforeShow
+
+//Custom Code @193-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close Page_BeforeShow @1-4BC230CD
+    return $Page_BeforeShow;
+}
+//End Close Page_BeforeShow
+
+?>
