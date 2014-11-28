fcc8ac88d1b75d25e91b2411e60a4a4dacb73247
wiliamdecosta@gmail.com <wiliamdecosta@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-11-28 11:54:55 +0000

    ''

--- trunk/report/cetak_rep_pengurangan_bphtb_gono_gini.php
+++ trunk/report/cetak_rep_pengurangan_bphtb_gono_gini.php
@@ -13,16 +13,23 @@ $t_bphtb_registration_id		= CCGetFromGet("t_bphtb_registration_id", "");
 $user				= CCGetUserLogin();
 $data				= array();
 $dbConn				= new clsDBConnSIKP();
-$query				= "select a.*,
-b.region_name as wp_region,
-c.region_name as wp_region_kec,
-d.region_name as wp_region_kel,
+$query				= "select j.t_bphtb_exemption_id, j.exemption_amount, j.dasar_pengurang, j.analisa_penguranan, j.jenis_pensiunan, j.jenis_perolehan_hak, j.sk_bpn_no, to_char(j.tanggal_sk,'DD-MM-YYYY') as tanggal_sk, 
+j.pilihan_lembar_cetak, j.opsi_a2, j.opsi_a2_keterangan, j.opsi_b7, j.opsi_b7_keterangan, j.keterangan_opsi_c, j.keterangan_opsi_c_gono_gini,
+to_char(j.tanggal_berita_acara,'DD-MM-YYYY') as tanggal_berita_acara, j.pemeriksa_id, j.administrator_id,
+k.pemeriksa_nama as nama_pemeriksa, k.pemeriksa_nip as nip_pemeriksa, k.pemeriksa_jabatan as jabatan_pemeriksa,
+l.pemeriksa_nama as nama_operator, l.pemeriksa_nip as nip_operator, l.pemeriksa_jabatan as jabatan_operator,
+a.*,
+cust_order.p_rqst_type_id,
+b.region_name as wp_kota,
+c.region_name as wp_kecamatan,
+d.region_name as wp_kelurahan,
 e.region_name as object_region,
-f.region_name as object_region_kec,
-g.region_name as object_region_kel,
+f.region_name as object_kecamatan,
+g.region_name as object_kelurahan,
 h.description as doc_name
 
-from t_bphtb_registration as a 
+from t_bphtb_exemption as j
+left join t_bphtb_registration as a  on j.t_bphtb_registration_id = a.t_bphtb_registration_id
 left join p_region as b
 	on a.wp_p_region_id = b.p_region_id
 left join p_region as c
@@ -37,7 +44,13 @@ left join p_region as g
 	on a.object_p_region_id_kel = g.p_region_id
 left join p_bphtb_legal_doc_type as h
 	on a.p_bphtb_legal_doc_type_id = h.p_bphtb_legal_doc_type_id
-where a.t_bphtb_registration_id = $t_bphtb_registration_id";
+left join t_customer_order as cust_order
+	on cust_order.t_customer_order_id = a.t_customer_order_id
+left join t_bphtb_exemption_pemeriksa as k
+   on j.pemeriksa_id = k.t_bphtb_exemption_pemeriksa_id
+left join t_bphtb_exemption_pemeriksa as l
+	on j.administrator_id = l.t_bphtb_exemption_pemeriksa_id
+where j.t_bphtb_registration_id = $t_bphtb_registration_id";
 
 $dbConn->query($query);
 while ($dbConn->next_record()) {
@@ -46,16 +59,16 @@ while ($dbConn->next_record()) {
 	$data["wp_address_name"]		= $dbConn->f("wp_address_name");
 	$data["wp_rt"]					= $dbConn->f("wp_rt");
 	$data["wp_rw"]					= $dbConn->f("wp_rw");
-	$data["wp_region"]				= $dbConn->f("wp_region");
-	$data["wp_region_kec"]			= $dbConn->f("wp_region_kec");
-	$data["wp_region_kel"]			= $dbConn->f("wp_region_kel");
+	$data["wp_region"]				= $dbConn->f("wp_kota");
+	$data["wp_region_kec"]			= $dbConn->f("wp_kecamatan");
+	$data["wp_region_kel"]			= $dbConn->f("wp_kelurahan");
 	$data["njop_pbb"]				= $dbConn->f("njop_pbb");
 	$data["object_address_name"]	= $dbConn->f("object_address_name");
 	$data["object_rt"]				= $dbConn->f("object_rt");
 	$data["object_rw"]				= $dbConn->f("object_rw");
 	$data["object_region"]			= $dbConn->f("object_region");
-	$data["object_region_kec"]		= $dbConn->f("object_region_kec");
-	$data["object_region_kel"]		= $dbConn->f("object_region_kel");
+	$data["object_region_kec"]		= $dbConn->f("object_kecamatan");
+	$data["object_region_kel"]		= $dbConn->f("object_kelurahan");
 	$data["doc_name"]				= $dbConn->f("doc_name");
 	$data["land_area"]				= $dbConn->f("land_area");
 	$data["land_price_per_m"]		= $dbConn->f("land_price_per_m");
@@ -71,10 +84,42 @@ while ($dbConn->next_record()) {
 	$data["bphtb_discount"]			= $dbConn->f("bphtb_discount");
 	$data["bphtb_amt_final"]		= $dbConn->f("bphtb_amt_final");
 	$data["registration_no"]		= $dbConn->f("registration_no");
-	$data["verificated_by"]			= $dbConn->f("verificated_by");
-	$data["verificated_nip"]		= $dbConn->f("verificated_nip");
 	$data["jenis_harga_bphtb"]		= $dbConn->f("jenis_harga_bphtb");
 	$data["description"]			= $dbConn->f("description");
+	$data["exemption_amount"]		= $dbConn->f("exemption_amount");
+	$data["dasar_pengurang"]		= $dbConn->f("dasar_pengurang");
+	$data["analisa_penguranan"]		= $dbConn->f("analisa_penguranan");
+	$data["nama_pemeriksa"]		    = $dbConn->f("nama_pemeriksa");
+	$data["jabatan_pemeriksa"]		= $dbConn->f("jabatan_pemeriksa");
+	$data["nip_pemeriksa"]		    = $dbConn->f("nip_pemeriksa");
+	$data["nama_operator"]		    = $dbConn->f("nama_operator");
+	$data["jabatan_operator"]		= $dbConn->f("jabatan_operator");
+	$data["nip_operator"]		    = $dbConn->f("nip_operator");
+	$data["jenis_pensiunan"]		= $dbConn->f("jenis_pensiunan");
+	$data["sk_bpn_no"]		        = $dbConn->f("sk_bpn_no");
+	$data["tanggal_sk"]		        = $dbConn->f("tanggal_sk");
+	$data["persen_pengurangan"]     = ceil($dbConn->f("bphtb_discount")/$dbConn->f("bphtb_amt") * 100);
+	$data["jenis_perolehan_hak"]	= $dbConn->f("jenis_perolehan_hak");
+	$data["pilihan_lembar_cetak"]	= $dbConn->f("pilihan_lembar_cetak");
+	$data["opsi_a2"]	            = $dbConn->f("opsi_a2");
+	$data["opsi_a2_keterangan"]	    = $dbConn->f("opsi_a2_keterangan");
+	$data["opsi_b7"]	            = $dbConn->f("opsi_b7");
+	$data["opsi_b7_keterangan"]	    = $dbConn->f("opsi_b7_keterangan");
+	$data["keterangan_opsi_c"]	    = $dbConn->f("keterangan_opsi_c");
+	$data["keterangan_opsi_c_gono_gini"]	    = $dbConn->f("keterangan_opsi_c_gono_gini");
+	
+	$data["land_area_recount"]				= $dbConn->f("land_area") / 2;
+	$data["land_price_per_m_recount"]		= $dbConn->f("land_price_per_m");
+	$data["land_total_price_recount"]		= ($data["land_area_recount"] * $data['land_price_per_m_recount']);
+	$data["building_area_recount"]			= $dbConn->f("building_area") / 2;
+	$data["building_price_per_m_recount"]	= $dbConn->f("building_price_per_m");
+	$data["building_total_price_recount"]	= ($data["building_area_recount"] * $data['building_price_per_m_recount']);
+	
+	$data["npop_recount"]					= ($data["land_total_price_recount"] + $data["building_total_price_recount"]);
+	$data["npop_kp_recount"]				= abs($data['npop_recount'] - $data["npop_tkp"]);
+	$data["bphtb_amt_recount"]				= abs(5/100 * $data["npop_kp_recount"]);
+	$data["bphtb_amt_final_recount"]		= abs($data["persen_pengurangan"]/100 * $data["bphtb_amt_recount"]);
+	
 }
 
 $dbConn->close();
@@ -118,11 +163,11 @@ class FormCetak extends FPDF {
 		$this->AddPage("P");
 		$encImageData = '';
 		$dbConn = new clsDBConnSIKP();
-		$query = "select f_encrypt_str('".$data['registration_no']."') AS enc_data";
-
+		$query = "SELECT * FROM f_terbilang('".ceil($data['bphtb_amt_final_recount'])."','') as terbilang";
 		$dbConn->query($query);
-		while ($dbConn->next_record()) {
-			$encImageData = $dbConn->f("enc_data");
+		$data['terbilang'] = '';
+		if($dbConn->next_record()) {
+		    $data['terbilang'] = ucwords($dbConn->f("terbilang"))." Rupiah";
 		}
 		//$this->Image('../images/logo_pemda.png',20,10,25,25);
 		//$this->Image('http://'.$_SERVER['HTTP_HOST'].'/mpd/include/qrcode/generate-qr.php?param='.$encImageData,165,10,25,25,'PNG');
@@ -152,7 +197,7 @@ class FormCetak extends FPDF {
 
 		$this->SetFont("Arial", "B", 12);
 		$this->barisBaru("A", "1 Nama Wajib Pajak", ": " . $data["wp_name"]);
-		$this->barisBaru("", "2 PNS/Pensiunan PNS", ": Departemen Dalam Negeri");
+		$this->barisBaru("", "2 ".$data['opsi_a2'], ": ".$data['opsi_a2_keterangan']);
 		$this->barisBaru("", "3 Alamat Wajib Pajak", ": " . $data["wp_address_name"]);
 		$this->barisBaru("", "4 RT/RW", ": RT. " . $data["wp_rt"] . "/RW. " .  $data["wp_rw"]);
 		$this->barisBaru("", "5 Kelurahan/Desa", ": " . $data["wp_region_kel"]);
@@ -166,7 +211,7 @@ class FormCetak extends FPDF {
 		$this->barisBaru("", "4 Kelurahan/Desa", ": " . $data["object_region_kel"]);
 		$this->barisBaru("", "5 Kecamatan", ": " . $data["object_region_kec"]);
 		$this->barisBaru("", "6 Kabupaten/Kota", ": " . $data["object_region"]);
-		$this->barisBaru("", "7 SK BPN No. ", ": 291/HM/BPN 32.73/2014");
+		$this->barisBaru("", "7 ".$data['opsi_b7'], ": ".$data['opsi_b7_keterangan']);
 		$this->Ln();
 		
 		$this->barisBaru("C", "Penghitungan NJOP PBB", "");
@@ -203,8 +248,8 @@ class FormCetak extends FPDF {
 		$this->Cell($lbodyx1, $this->height, number_format($data["land_price_per_m"], 0, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_total_price"], 0, ",", "."), "", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["land_total_price"], 0, ",", "."), "R", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "R", 0, "");*/
 		$this->Ln();
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
@@ -215,8 +260,8 @@ class FormCetak extends FPDF {
 		$this->Cell($lbodyx1, $this->height, number_format($data["building_price_per_m"], 0, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["building_total_price"], 0, ",", "."), "", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["building_total_price"], 0, ",", "."), "R", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "R", 0, "");*/
 		$this->Ln();
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
@@ -227,8 +272,8 @@ class FormCetak extends FPDF {
 		$this->Cell($lbodyx1, $this->height, "", "B", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "BR", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "Rp", "B", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_total_price"] + $data["building_total_price"], 0, ",", "."), "B", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "BR", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["land_total_price"] + $data["building_total_price"], 0, ",", "."), "BR", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "BR", 0, "");*/
 		$this->Ln();
 		$jenis_harga_bphtb = $data["jenis_harga_bphtb"];
 		if(empty($jenis_harga_bphtb)) $jenis_harga_bphtb = 99;
@@ -241,7 +286,7 @@ class FormCetak extends FPDF {
 			array
 			(
 				"",
-				"Tanah dan bangunan tersebut merupakan Harta Bersama (Gono-Gini) Suami-Istri dimana Istri masih hidup dan suami telah meninggal berdasarkan Ps. 35 UU No. 1 Th. 1973 tentang Perkawinan, maka objek waris yang dikenakan BPHTB adalah bagian dari Suami yang telah meninggal, maka perhitungan NJOP-nya adalah sebagai berikut:"
+				$data['keterangan_opsi_c_gono_gini']
 			),
 			array
 			(
@@ -273,26 +318,26 @@ class FormCetak extends FPDF {
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Tanah (bumi)", "L", 0, "");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_area"], 0, ",", "."), "", 0, "R");
+		$this->Cell($lbodyx1, $this->height, number_format($data["land_area_recount"], 2, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "m2", "R", 0, "L");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_price_per_m"], 0, ",", "."), "", 0, "R");
+		$this->Cell($lbodyx1, $this->height, number_format($data["land_price_per_m_recount"], 0, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_total_price"], 0, ",", "."), "", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["land_total_price_recount"], 0, ",", "."), "R", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "R", 0, "");*/
 		$this->Ln();
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Bangunan", "L", 0, "");
-		$this->Cell($lbodyx1, $this->height, number_format($data["building_area"], 0, ",", "."), "", 0, "R");
+		$this->Cell($lbodyx1, $this->height, number_format($data["building_area_recount"], 2, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "m2", "R", 0, "L");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["building_price_per_m"], 0, ",", "."), "", 0, "R");
+		$this->Cell($lbodyx1, $this->height, number_format($data["building_price_per_m_recount"], 0, ",", "."), "", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
 		$this->Cell($lbodyx1, $this->height, "Rp", "", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["building_total_price"], 0, ",", "."), "", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "R", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["building_total_price_recount"], 0, ",", "."), "R", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "R", 0, "");*/
 		$this->Ln();
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
@@ -303,23 +348,23 @@ class FormCetak extends FPDF {
 		$this->Cell($lbodyx1, $this->height, "", "B", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "", "BR", 0, "R");
 		$this->Cell($lbodyx1, $this->height, "Rp", "B", 0, "L");
-		$this->Cell($lbodyx1, $this->height, number_format($data["land_total_price"] + $data["building_total_price"], 0, ",", "."), "B", 0, "R");
-		$this->Cell($lbodyx1, $this->height, "", "BR", 0, "");
+		$this->Cell($lbodyx1+$lbodyx1, $this->height, number_format($data["land_total_price_recount"] + $data["building_total_price_recount"], 0, ",", "."), "BR", 0, "R");
+		/*$this->Cell($lbodyx1, $this->height, "", "BR", 0, "");*/
 		$this->Ln();
 
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
-		$this->Cell($this->length - $lbody1, $this->height, "Jenis perolehan hak atas tanah dan atau bangunan	"."Rumah Dinas", "", 0, "");
+		$this->Cell($this->length - $lbody1, $this->height, "Jenis perolehan hak atas tanah dan atau bangunan	".$data['jenis_perolehan_hak'], "", 0, "");
 		$this->Ln();
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
-		$this->Cell($this->length - $lbody1, $this->height, "SK Kepala Kantor Pertanahan Kota Bandung", "", 0, "");
+		$this->Cell($this->length - $lbody1, $this->height, $data['keterangan_opsi_c'], "", 0, "");
 		$this->Ln();
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbody1+$lbody1, $this->height, "Nomor ", "", 0, "");
-		$this->Cell($this->length - $lbody1 -$lbody1-$lbody1, $this->height, ": 291/HM/BPN 32.73/2014", "", 0, "");
+		$this->Cell($this->length - $lbody1 -$lbody1-$lbody1, $this->height, ": ".$data['opsi_b7_keterangan'], "", 0, "");
 		$this->Ln();
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbody1+ $lbody1, $this->height, "Tanggal ", "", 0, "");
-		$this->Cell($this->length - $lbody1-$lbody1-$lbody1, $this->height, ": 08 Oktober 2014", "", 0, "");
+		$this->Cell($this->length - $lbody1-$lbody1-$lbody1, $this->height, ": ".$this->beautyDate($data['tanggal_sk']), "", 0, "");
 		$this->Ln(7);
 		
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
@@ -341,9 +386,9 @@ class FormCetak extends FPDF {
 		}
 
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
-		$this->Cell($this->length - $lbody1, $this->height, "Pengenaan Pengurangan Karena "."Rumah Dinas", "", 0, "");
+		$this->Cell($this->length - $lbody1, $this->height, "Pengenaan Pengurangan Karena ".$data['jenis_perolehan_hak'], "", 0, "");
 		$this->Ln();
-		$this->barisBaru2($lbody1, "Besaran Pengenaan Pengurangan ", "50%", "Rp", $data["bphtb_discount"]);
+		$this->barisBaru2($lbody1, "Besaran Pengenaan Pengurangan ", $data['persen_pengurangan']."%", "Rp", $data["bphtb_discount"]);
 
 		if($data["npop_kp"]==0){
 			$this->barisBaruStr($lbody1, "Bea Perolehan Hak atas Tanah dan Bangunan yang harus dibayar", "", "Rp", "NIHIL");
@@ -354,7 +399,7 @@ class FormCetak extends FPDF {
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbody1+ $lbody1, $this->height, "Terbilang :", "", 0, "");
 		$this->SetFont("Arial", "iB", 8);
-		$this->Cell($this->length - $lbody1- $lbody1- $lbody1, $this->height, "Sekian Juta Sekian ratus ribu sekian puluh ribu sekian ribu sekian ratus rupiah", "", 0, "");
+		$this->Cell($this->length - $lbody1- $lbody1- $lbody1, $this->height, $data['terbilang'], "", 0, "");
 		
 		
 		$this->Ln(10);
@@ -362,6 +407,17 @@ class FormCetak extends FPDF {
 		$this->Cell($lbody1+5, $this->height, "Catatan :", "", 0, "");
 		$this->SetFont("Arial", "i", 8);
 		$this->Cell($this->length - $lbody1 - 5, $this->height, "Apabila ada kekeliruan dikarenakan data maka akan diperbaiki dengan data terbaru", "", 0, "");
+	}
+	
+	function beautyDate($tgl) {
+	    
+	    $arrtgl = explode("-", $tgl);
+	    $dd = $arrtgl[0];
+	    $mm = $arrtgl[1];
+	    $yyyy = $arrtgl[2];
+	    
+	    $arrmonth = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+	    return $dd." ".$arrmonth[$mm-1]." ".$yyyy;
 	}
 	
 	function barisBaru3($subtractor, $field, $middle, $currency, $data){
