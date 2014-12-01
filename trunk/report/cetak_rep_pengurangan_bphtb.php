5f5a512e8c20729c60ce7c48aac88906f6da6542
wiliamdecosta@gmail.com <wiliamdecosta@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-12-01 04:10:21 +0000

    ''

--- trunk/report/cetak_rep_pengurangan_bphtb.php
+++ trunk/report/cetak_rep_pengurangan_bphtb.php
@@ -15,6 +15,7 @@ $dbConn				= new clsDBConnSIKP();
 $query				= "select j.t_bphtb_exemption_id, j.exemption_amount, j.dasar_pengurang, j.analisa_penguranan, j.jenis_pensiunan, j.jenis_perolehan_hak, j.sk_bpn_no, to_char(j.tanggal_sk,'DD-MM-YYYY') as tanggal_sk, 
 j.pilihan_lembar_cetak, j.opsi_a2, j.opsi_a2_keterangan, j.opsi_b7, j.opsi_b7_keterangan, j.keterangan_opsi_c, j.keterangan_opsi_c_gono_gini,
 to_char(j.tanggal_berita_acara,'DD-MM-YYYY') as tanggal_berita_acara, j.pemeriksa_id, j.administrator_id,
+j.nomor_berita_acara, j.nomor_notaris,
 k.pemeriksa_nama as nama_pemeriksa, k.pemeriksa_nip as nip_pemeriksa, k.pemeriksa_jabatan as jabatan_pemeriksa,
 l.pemeriksa_nama as nama_operator, l.pemeriksa_nip as nip_operator, l.pemeriksa_jabatan as jabatan_operator,
 a.*,
@@ -106,6 +107,8 @@ while ($dbConn->next_record()) {
 	$data["opsi_b7_keterangan"]	    = $dbConn->f("opsi_b7_keterangan");
 	$data["keterangan_opsi_c"]	    = $dbConn->f("keterangan_opsi_c");
 	$data["keterangan_opsi_c_gono_gini"]	    = $dbConn->f("keterangan_opsi_c_gono_gini");
+	$data["nomor_berita_acara"]	    = $dbConn->f("nomor_berita_acara");
+	$data["nomor_notaris"]	    = $dbConn->f("nomor_notaris");
 }
 
 $dbConn->close();
@@ -271,7 +274,7 @@ class FormCetak extends FPDF {
 		$this->Ln();
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbody1+$lbody1, $this->height, "Nomor ", "", 0, "");
-		$this->Cell($this->length - $lbody1 -$lbody1-$lbody1, $this->height, ": ".$data['opsi_b7_keterangan'], "", 0, "");
+		$this->Cell($this->length - $lbody1 -$lbody1-$lbody1, $this->height, ": ".$data['nomor_notaris'], "", 0, "");
 		$this->Ln();
 		$this->Cell($lbody1, $this->height, "", "", 0, "");
 		$this->Cell($lbody1+ $lbody1, $this->height, "Tanggal ", "", 0, "");
