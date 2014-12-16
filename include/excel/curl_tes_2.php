1be43b379d9b2df83b822bb062cd9a1353d05855
haxbox.keren <haxbox.keren@gmail.com>
2014-12-16 10:02:52 +0000

    ''

--- include/excel/curl_tes_2.php
+++ include/excel/curl_tes_2.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL ^ E_WARNING);
+error_reporting(E_ALL ^ E_WARNING ^ E_DEPRECATED);
 define("RelativePath", "../..");
 	define("PathToCurrentPage", "/include/excel/");	
 	define("FileName", "curl_tes_2.php"); 	
