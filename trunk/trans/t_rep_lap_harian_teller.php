472157fd632e6dc85420b742b7e32ffec26183e4
wiliamdecosta@gmail.com <wiliamdecosta@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2014-12-23 09:41:53 +0000

    ''

--- trunk/trans/t_rep_lap_harian_teller.php
+++ trunk/trans/t_rep_lap_harian_teller.php
@@ -45,7 +45,7 @@ class clsRecordt_rep_lap_harian_bdhrSearch { //t_rep_lap_harian_bdhrSearch Class
     // Class variables
 //End Variables
 
-//Class_Initialize Event @3-C013ED16
+//Class_Initialize Event @3-6A12F0F8
     function clsRecordt_rep_lap_harian_bdhrSearch($RelativePath, & $Parent)
     {
 
@@ -75,7 +75,7 @@ class clsRecordt_rep_lap_harian_bdhrSearch { //t_rep_lap_harian_bdhrSearch Class
             $this->Button_DoSearch1 = & new clsButton("Button_DoSearch1", $Method, $this);
             $this->nama_teller = & new clsControl(ccsListBox, "nama_teller", "nama_teller", ccsText, "", CCGetRequestParam("nama_teller", $Method, NULL), $this);
             $this->nama_teller->DSType = dsListOfValues;
-            $this->nama_teller->Values = array(array("ESSYBKP", "ESSYBKP"), array("HERIBKP", "HERIBKP"), array("PAINCEBKP", "PAINCEBKP"), array("ABDURACHIM", "ABDURACHIM"));
+            $this->nama_teller->Values = array(array("ESSYBKP", "ESSYBKP"), array("HERIBKP", "HERIBKP"), array("PAINCEBKP", "PAINCEBKP"));
             $this->Button_DoSearch2 = & new clsButton("Button_DoSearch2", $Method, $this);
             $this->Button_DoSearch3 = & new clsButton("Button_DoSearch3", $Method, $this);
         }
