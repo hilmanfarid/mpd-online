e43361dec1bb4dfc931307a920dab0ae76ee16f1
fajar.rusius <fajar.rusius@gmail.com>
2014-12-29 08:30:37 +0000

    ''

--- send_email.php
+++ send_email.php
@@ -1,12 +1,12 @@
 <?php
     require_once 'include/swift/lib/swift_required.php';           
-    $html.=    '<p>tes email
+    $html.=    '<p>tes email in var html
     
             </p></br>';     
     $mailConfig = array(
         'host_smtp' => 'smtp.gmail.com',
-        'username' => 'user',
-        'password' => 'password'
+        'username' => 'testmpd2014@gmail.com',
+        'password' => 'mpdonline'
     );
     
     $transport = Swift_SmtpTransport::newInstance($mailConfig['host_smtp'], 465, "ssl")
@@ -15,10 +15,18 @@
     
     $mailer = Swift_Mailer::newInstance($transport);
     
-    $message = Swift_Message::newInstance('BOSS BANDUNG')
-      ->setFrom(array($mailConfig['username'] => 'BOSS'))
+    $message = Swift_Message::newInstance('MPD ONLINE')//SUBJECT
+      ->setFrom(array($mailConfig['username'] => 'DINAS PELAYANAN PAJAK'))//NAME APPEAR IN INBOX (sender's name)
       ->setTo($_GET['receiver'])
-      ->setBody($_GET['message'], 'text/html');/*->attach(
+      //->setBody($_GET['message'], 'text/html');
+	  ->setBody($html, 'text/html');
+	  /*->attach(
     Swift_Attachment::fromPath('var/attachment/image.jpg')->setFilename('myfilename.jpg')
     );*/
     $result = $mailer->send($message);
+	if ($result==1){
+	echo 'mail sent';
+	}else{
+	echo 'failed sending email';
+	}
+	
