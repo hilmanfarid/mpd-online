f7cc6ef1a50ef2007ea5fc9bc4d6933dba865c0e
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2015-01-21 02:54:18 +0000

    ''

--- trunk/report/cetak_formulir_surat_teguran_pdf.php
+++ trunk/report/cetak_formulir_surat_teguran_pdf.php
@@ -4,6 +4,7 @@ define("PathToCurrentPage", "/report/");
 define("FileName", "cetak_formulir_surat_teguran_pdf.php");
 include_once(RelativePath . "/Common.php");
 include_once("../include/fpdf.php");
+include_once("../include/qrcode/generate-qr-file.php");
 
 $t_customer_order_id = CCGetFromGet("t_customer_order_id", "");
 
@@ -63,6 +64,9 @@ while ($dbConn->next_record()) {
 	
 $dbConn->close();
 }
+
+//$path = '';
+//generate_qr($param,$path);
 
 class FormCetak extends FPDF {
 	var $fontSize = 10;
@@ -457,24 +461,33 @@ class FormCetak extends FPDF {
 		$this->Ln();
 		$this->Cell($this->lengthCell, $this->height, "", "LR", 0, 'L');
 		$this->Ln();
-
-		$this->Image('http://'.$_SERVER['HTTP_HOST'].'/mpd/include/qrcode/generate-qr.php?param='.
-		str_replace(" ","-",$data['letter_date_txt'])."_".
-		$data["npwd"]."_".
-		str_replace(" ","-",$data["periode"])
-		,15,170,25,25,'PNG');
+		
+		if ($_SERVER['HTTP_HOST']=='172.16.20.1'){
+			$this->Image('http://'.$_SERVER['HTTP_HOST'].'/include/qrcode/generate-qr.php?param='.
+			str_replace(" ","-",$data['letter_date_txt'])."_".
+			$data["npwd"]."_".
+			str_replace(" ","-",$data["periode"])
+			,15,170,25,25,'PNG');
+		}else{
+			$this->Image('http://'.$_SERVER['HTTP_HOST'].'/mpd/include/qrcode/generate-qr.php?param='.
+			str_replace(" ","-",$data['letter_date_txt'])."_".
+			$data["npwd"]."_".
+			str_replace(" ","-",$data["periode"])
+			,15,170,25,25,'PNG');
+		}
+		
 		
 		$this->Cell($lbody2, $this->height, "", "L", 0, 'C');
 		$this->Cell($lbody4, $this->height, "", "", 0, 'L');
 		$this->Cell($lbody4-5, $this->height, "", "", 0, 'C');
-		$this->Cell($lbody4+10, $this->height, "H. SONI BAKHTIYAR, S.Sos, M.Si", "B", 0, 'C');
+		$this->Cell($lbody4+10, $this->height, "Drs. H. GUN GUN SUMARYANA", "B", 0, 'C');
 		$this->Cell($lbody2-5, $this->height, "", "R", 0, 'C');
 		$this->Ln();
 		
 		$this->Cell($lbody2, $this->height, "", "L", 0, 'C');
 		$this->Cell($lbody4, $this->height, "", "", 0, 'L');
 		$this->Cell($lbody4, $this->height, "", "", 0, 'C');
-		$this->Cell($lbody4 - 2, $this->height, "NIP. 19750625 199403 1 001", "", 0, 'C'); //isi nip
+		$this->Cell($lbody4 - 2, $this->height, "NIP. 19700806 199101 1 001", "", 0, 'C'); //isi nip
 		$this->Cell(2, $this->height, "", "", 0, 'L');
 		$this->Cell($lbody2, $this->height, "", "R", 0, 'C');
 		$this->Ln();
