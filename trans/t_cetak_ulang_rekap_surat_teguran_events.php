483a4d73eb5c98e3e2c40bd038066943a5931cb8
fajar.rusius <fajar.rusius@gmail.com>
2015-02-13 08:46:43 +0000

    ''

---
+++ trans/t_cetak_ulang_rekap_surat_teguran_events.php
@@ -0,0 +1,31 @@
+<?php
+//BindEvents Method @1-FA3AC75D
+function BindEvents()
+{
+    global $CCSEvents;
+    $CCSEvents["OnInitializeView"] = "Page_OnInitializeView";
+}
+//End BindEvents Method
+
+//Page_OnInitializeView @1-856EB6F4
+function Page_OnInitializeView(& $sender)
+{
+    $Page_OnInitializeView = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_cetak_ulang_rekap_surat_teguran; //Compatibility
+//End Page_OnInitializeView
+
+//Custom Code @66-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close Page_OnInitializeView @1-81DF8332
+    return $Page_OnInitializeView;
+}
+//End Close Page_OnInitializeView
+
+
+?>
