6936589b9036e25815a0e16edd9037a0e189b8cb
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2015-02-26 05:49:18 +0000

    ''

---
+++ trunk/send_email_forgot_password.php
@@ -0,0 +1,33 @@
+<?php
+    require_once 'include/swift/lib/swift_required.php';               
+    $mailConfig = array(
+        'host_smtp' => 'smtp.gmail.com',
+        'username' => 'helpdesk@disyanjak.net',
+        'password' => 'disyanjakbdg'
+    );
+    
+    $transport = Swift_SmtpTransport::newInstance($mailConfig['host_smtp'], 465, "ssl")
+      ->setUsername($mailConfig['username'])
+      ->setPassword($mailConfig['password']);
+    
+    $mailer = Swift_Mailer::newInstance($transport);
+    
+	$html.= '<p>Anda telah mengajukan permintaan pergantian password pada aplikasi wp.disyanjak.net. 
+	Username dan Password yang baru untuk akun Anda adalah sebagai berikut:</p>
+	<p>	username : '.$_GET['username'].'</p>
+	<p>	password baru : '.$_GET['password'].'</p>';
+    $message = Swift_Message::newInstance('PENGGANTIAN PASSWORD')//SUBJECT
+      ->setFrom(array($mailConfig['username'] => 'DINAS PELAYANAN PAJAK KOTA BANDUNG'))//NAME APPEAR IN INBOX (sender's name)
+      ->setTo($_GET['receiver'])
+      //->setBody($_GET['message'], 'text/html');
+	  ->setBody($html, 'text/html');
+	  /*->attach(
+    Swift_Attachment::fromPath('var/attachment/image.jpg')->setFilename('myfilename.jpg')
+    );*/
+    $result = $mailer->send($message);
+	if ($result==1){
+	echo 'mail sent';
+	}else{
+	echo 'failed sending email';
+	}
+	
