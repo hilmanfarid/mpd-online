343ee7177c36172f8309db0491b2ca6cdfd0affe
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2015-03-23 04:31:29 +0000

    ''

---
+++ trunk/report/cek_pembayaran_terakhir_wp.php
@@ -0,0 +1,32 @@
+<?php
+define("RelativePath", "..");
+define("PathToCurrentPage", "/report/");
+define("FileName", "cetak_formulir_surat_teguran_pdf.php");
+include_once(RelativePath . "/Common.php");
+
+$npwpd = CCGetFromGet("npwpd", "");
+
+$dbConn = new clsDBConnSIKP();
+
+$query="select * from t_payment_receipt where npwd = upper('".$npwpd."')
+		ORDER BY payment_date desc limit 1";
+
+$dbConn->query($query);
+$data=array();
+if ($dbConn->next_record()) {
+	$json = array('items'=>array(),'message'=>'data ditemukan','success'=>'success');
+	$item = array(
+		'npwd' => $dbConn->f("npwd"),
+		'payment_vat_amount' => $dbConn->f("payment_vat_amount"),
+		'finance_period_code' => $dbConn->f("finance_period_code")
+		);
+	$json['items']=$item;
+	echo json_encode($json);
+}else{
+	$json = array('items'=>array(),'message'=>'data tidak ditemukan','success'=>'fail');
+	echo json_encode($json);
+}
+$dbConn->close();
+
+
+?>
