5a484c1e57e169ae479adf1f4d5c01eb09c761d5
fajar.rusius <fajar.rusius@gmail.com>
2015-03-24 09:21:57 +0000

    ''

--- report/cek_pembayaran_terakhir_wp.php
+++ report/cek_pembayaran_terakhir_wp.php
@@ -14,17 +14,31 @@ $query="select * from t_payment_receipt where npwd = upper('".$npwpd."')
 $dbConn->query($query);
 $data=array();
 if ($dbConn->next_record()) {
-	$json = array('items'=>array(),'message'=>'data ditemukan','success'=>'success');
+	/*$json = array('items'=>array(),'message'=>'data ditemukan','success'=>'success');
 	$item = array(
 		'npwd' => $dbConn->f("npwd"),
 		'payment_vat_amount' => $dbConn->f("payment_vat_amount"),
 		'finance_period_code' => $dbConn->f("finance_period_code")
 		);
 	$json['items']=$item;
-	print_r( ($json));
+	print_r( ($json));*/
+	$myXMLData =
+	"<?xml version='1.0' encoding='UTF-8'?>
+	<pembayaran_wp>
+		<npwd>".$dbConn->f("npwd")."</npwd>
+		<payment_vat_amount>".$dbConn->f("payment_vat_amount")."</payment_vat_amount>
+		<finance_period_code>".$dbConn->f("finance_period_code")."</finance_period_code>
+	</pembayaran_wp>";
+	print_r($myXMLData);
 }else{
-	$json = array('items'=>array(),'message'=>'data tidak ditemukan','success'=>'fail');
-	echo json_encode($json);
+	$myXMLData =
+	"<?xml version='1.0' encoding='UTF-8'?>
+	<pembayaran_wp>
+		<npwd></npwd>
+		<payment_vat_amount></payment_vat_amount>
+		<finance_period_code></finance_period_code>
+	</pembayaran_wp>";
+	print_r($myXMLData);
 }
 $dbConn->close();
 
