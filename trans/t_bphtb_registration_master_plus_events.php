57e987b54bac98ffc195e61ac4f78e74e0d3a6c9
fajar.rusius <fajar.rusius@gmail.com>
2015-03-25 04:20:46 +0000

    ''

--- trans/t_bphtb_registration_master_plus_events.php
+++ trans/t_bphtb_registration_master_plus_events.php
@@ -1,10 +1,11 @@
 <?php
-//BindEvents Method @1-1F9A752D
+//BindEvents Method @1-9362233F
 function BindEvents()
 {
     global $t_bphtb_registrationForm;
     global $CCSEvents;
     $t_bphtb_registrationForm->Button3->CCSEvents["OnClick"] = "t_bphtb_registrationForm_Button3_OnClick";
+    $t_bphtb_registrationForm->Button4->CCSEvents["OnClick"] = "t_bphtb_registrationForm_Button4_OnClick";
     $t_bphtb_registrationForm->CCSEvents["BeforeSelect"] = "t_bphtb_registrationForm_BeforeSelect";
     $t_bphtb_registrationForm->CCSEvents["BeforeInsert"] = "t_bphtb_registrationForm_BeforeInsert";
     $t_bphtb_registrationForm->ds->CCSEvents["AfterExecuteDelete"] = "t_bphtb_registrationForm_ds_AfterExecuteDelete";
@@ -32,6 +33,26 @@ function t_bphtb_registrationForm_Button3_OnClick(& $sender)
     return $t_bphtb_registrationForm_Button3_OnClick;
 }
 //End Close t_bphtb_registrationForm_Button3_OnClick
+
+//t_bphtb_registrationForm_Button4_OnClick @1061-A549D8E9
+function t_bphtb_registrationForm_Button4_OnClick(& $sender)
+{
+    $t_bphtb_registrationForm_Button4_OnClick = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $t_bphtb_registrationForm; //Compatibility
+//End t_bphtb_registrationForm_Button4_OnClick
+
+//Custom Code @1062-2A29BDB7
+// -------------------------
+    // Write your own code here.
+// -------------------------
+//End Custom Code
+
+//Close t_bphtb_registrationForm_Button4_OnClick @1061-369DD06C
+    return $t_bphtb_registrationForm_Button4_OnClick;
+}
+//End Close t_bphtb_registrationForm_Button4_OnClick
 
 //t_bphtb_registrationForm_BeforeSelect @94-50B4A263
 function t_bphtb_registrationForm_BeforeSelect(& $sender)
