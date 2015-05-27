d14b4c1a0d5c0f0f73cd6ce9e18f7ddd0533b9f3
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2015-05-27 07:28:17 +0000

    ''

--- trunk/trans/t_vat_setllement_manual_skpdkb_events.php
+++ trunk/trans/t_vat_setllement_manual_skpdkb_events.php
@@ -50,20 +50,66 @@ function t_vat_setllementForm_Button1_OnClick(& $sender)
 	if(empty($kamar)) $kamar = 0;	
 
 	$sql = "select * from f_vat_settlement_manual_skpdkb($cusAccId,$Period,'$npwd','$ms_start','$ms_end',$kamar,$tot,$p_vat_type_dtl_id,$p_vat_type_dtl_cls_id,'$User')";
-	
+	//echo $sql;exit;
+	//die($sql);
 	$dbConn->query($sql);
 	$dbConn->next_record();
 	$cust_id = $dbConn->f("o_cust_order_id");
 	$mess = $dbConn->f("o_mess");
 
 	//modified by wiliam
-	echo "<script> 
+	/*echo "<script> 
 		alert('".$mess."');
 		window.opener.location.reload();
 		window.close();
 	</script>";
 	
-	exit;
+	exit;*/
+	if(!$cust_id == 0 && !$cust_id == ""){
+		
+		$sql2 = "select * from f_first_submit_engine_2step(
+		501,
+		$cust_id,
+		'$User')";
+
+		//die($sql2);
+		$dbConn->query($sql2);
+		$dbConn->next_record();
+		$o_result_code = $dbConn->f("o_result_code");
+
+		if($o_result_code == 0){
+			echo '<script language="javascript">';
+			echo 'alert("'.$mess.'");';
+			echo '</script>';
+		}
+		else{
+			$mess = $dbConn->f("o_result_msg");
+			echo '<script language="javascript">';
+			echo 'alert("'.$mess.'");';
+			echo '</script>';
+		}
+	}
+	else{
+	    $mess = str_replace('"','',$mess);
+		echo '<script language="javascript">';
+		echo 'alert("'.$mess.'");';
+		echo '</script>';
+		
+	}
+	
+	if(!$cust_id == 0 && !$cust_id == ""){
+		$dbConn2 = new clsDBConnSIKP();
+		$sqlPayment = "select t_vat_setllement_id from t_vat_setllement where t_customer_order_id = ".$cust_id;
+		$dbConn2->query($sqlPayment);
+		$dbConn2->next_record();
+		$t_vat_setllement_id = $dbConn2->f("t_vat_setllement_id");
+	  	
+		echo '<script language="javascript">';
+		echo "window.open('http://localhost/mpd/report/cetak_formulir_sptpd.php?t_vat_setllement_id=".$t_vat_setllement_id."','No Payment', 'left=0,top=0,width=500,height=500,toolbar=no,scrollbars=yes,resizable=yes')";
+		echo '</script>';
+		
+	}
+	return;
 	
 // -------------------------
 
