b966266b807ad16c1e14c3b9ff8153b65afd95aa
fajar.rusius <fajar.rusius@gmail.com>
2015-06-22 03:56:55 +0000

    ''

--- report/direct.php
+++ report/direct.php
@@ -1,18 +1,35 @@
 <?php
-	include('../include/fpdf17/mc_table.php');
+	define("RelativePath", "..");
+	define("PathToCurrentPage", "/report/");
+	define("FileName", "cetak_surat_pengukuhan_pdf.php");
+	include_once(RelativePath . "/Common2.php");
+	include_once("../include/fpdf.php");
+	/*include('../include/fpdf17/mc_table.php');
 	$paperpdf = new PDF_MC_Table; 
 	// Create a new FPDF instance and set parameters  
 	$paperpdf->AddPage('P');  
 	$paperpdf->SetMargins(0,0,0);  
 	$paperpdf->SetFont('Arial','','10');  
 	$paperpdf->SetXY(50,20);  
-	$paperpdf->Write(5,'I will print when opened.');  
+	$paperpdf->Write(5,'Test Print.');  
 	$name_of_file = "\print_pdf_".time().".pdf";  
+	$paperpdf->Output('D:\work\list_pdf'.$name_of_file,'F');  */
 	// Add the javascript with parameters to the Adobe API print method to do silent printing  
 	// $paperpdf->IncludeJS("this.print({bUI: false, bSilent: true, bShrinkToFit: true});");  
-	  
+	$dbConn = new clsDBConnSIKP();
+	$dbConn2 = new clsDBConnSIKP();
+	$files1 = scandir('D:\work\list_pdf');  
+	$dbConn->query("select * from t_print_queue where status='SAVED'");
+	while ($dbConn->next_record()) {
+		$item = $dbConn->Record;
+		if(is_file('D:\work\list_pdf\\'.$item['file_name'])){
+			echo shell_exec('AcroRd32.exe /n /t "D:\work\list_pdf\\'.$item['file_name'].'" "HP LaserJet 200 color M251 PCL 6" "HP LaserJet 200 color M251 PCL 6" "172.16.20.203"');
+			//@unlink('D:\work\list_pdf\\'.$item);
+			$dbConn2->query("update t_print_queue set status='PRINTED' where t_customer_order_id=".$item['t_customer_order_id']);
+		}
+	}
 	// Have FPDF create and stream the auto-printing PDF to the browser  
-	$paperpdf->Output('D:\work\list_pdf'.$name_of_file,'F');  
-	shell_exec('AcroRd32.exe /n /t "D:\work\list_pdf'.$name_of_file.'" "Send To OneNote 2010" "Send To OneNote 2010" "127.0.0.1"');
-	echo 'AcroRd32.exe /t "D:\work\list_pdf'.$name_of_file.'" "Send To OneNote 2010" "Send To OneNote 2010" "127.0.0.1"';
+	
+	
+	//echo 'AcroRd32.exe /t "D:\work\list_pdf'.$name_of_file.'" "HP LaserJet 200 color M251 PCL 6" "HP LaserJet 200 color M251 PCL 6" "172.16.20.203"';
 ?>
