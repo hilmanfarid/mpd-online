5c773bbf295fe5695f637eade4b7c6134ca08cdd
fajar.rusius@gmail.com <fajar.rusius@gmail.com@e731989e-7ee1-b65d-8241-ca05f562bc65>
2015-06-25 07:58:24 +0000

    ''

--- trunk/check_open_session.php
+++ trunk/check_open_session.php
@@ -2,7 +2,7 @@
 	$check_user_id = CCGetUserID();
 	if(empty($check_user_id)){
 		echo "<script>
-			top.top.location.href='index.php'
+			top.top.location.href='".$_SERVER["DOCUMENT_ROOT"]."'/mpd
 		</script>
 		";
 		exit;
