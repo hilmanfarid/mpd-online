b7a0d79d82e062d4a0026935f5f59b2e7a196d57
haxbox.keren <haxbox.keren@gmail.com>
2015-06-26 06:42:17 +0000

    ''

--- auto_login.php
+++ auto_login.php
@@ -1,5 +1,5 @@
 <?php
-	//echo urlencode('http://172.16.20.3:81/mpd-development/workflow/workflow_summary.php?ELEMENT_ID=1050002017&P_W_DOC_TYPE_ID=500&P_W_PROC_ID=2&PROFILE_TYPE=INBOX&P_APP_USER_ID=17&sumworkflowDir=Asc&sumworkflowPageSize=50');
+	//echo base64_encode('Anda Mendapatkan 1 Pekerjaan belum disubmit :');
 	//exit;
 ?>
 <html>
@@ -7,7 +7,7 @@
 		<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 	</head>
 	<body>
-		<form id="Login" name="Login" action="main/sikp_login.php?ccsForm=Login" method="post">
+		<form id="Login" name="Login" action="main/sikp_login.php?ccsForm=Login&redirect=<?php echo $_GET['redirect'];?>" method="post">
 			<input class="input-login" value="<?php echo $_GET['username'];?>" name="redirect"><br>
 			<input class="input-login" value="<?php echo $_GET['username'];?>" name="login"><br>
 			<input class="input-login" style="BACKGROUND: url(../images/lock.png) buttonhighlight no-repeat left center; background-size: contain" type="password" value="<?php echo $_GET['password'];?>" name="password"><br>
