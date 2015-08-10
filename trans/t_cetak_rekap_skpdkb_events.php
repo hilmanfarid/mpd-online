93ea05088d43c6d675daad1b605adbf7e0f4ca04
fajar.rusius <fajar.rusius@gmail.com>
2015-08-10 05:15:47 +0000

    ''

--- trans/t_cetak_rekap_skpdkb_events.php
+++ trans/t_cetak_rekap_skpdkb_events.php
@@ -42,17 +42,20 @@ function Page_BeforeShow(& $sender)
     // Write your own code here.
 	$doAction = CCGetFromGet('doAction');
 	global $Label1;
-	if($doAction == 'view_html') {
-		$param_arr['p_year_period_id'] = CCGetFromGet('p_year_period_id');
-		$param_arr['p_finance_period_id'] = CCGetFromGet('p_finance_period_id');
-		$param_arr['p_vat_type_id'] = CCGetFromGet('p_vat_type_id');
-		$param_arr['status_bayar'] = CCGetFromGet('ListBox1');
-		$param_arr['npwpd'] = CCGetFromGet('npwpd');
+	$param_arr['p_year_period_id'] = CCGetFromGet('p_year_period_id');
+	$param_arr['p_finance_period_id'] = CCGetFromGet('p_finance_period_id');
+	$param_arr['p_vat_type_id'] = CCGetFromGet('p_vat_type_id');
+	$param_arr['status_bayar'] = CCGetFromGet('ListBox1');
+	$param_arr['npwpd'] = CCGetFromGet('npwpd');
 
-		$param_arr['vat_code'] = CCGetFromGet('vat_code');
-		$param_arr['year_code'] = CCGetFromGet('year_code');
-		$param_arr['code'] = CCGetFromGet('code');
+	$param_arr['vat_code'] = CCGetFromGet('vat_code');
+	$param_arr['year_code'] = CCGetFromGet('year_code');
+	$param_arr['code'] = CCGetFromGet('code');
+	if($doAction == 'view_html') {
 		$Label1->SetText(GetCetakHTML($param_arr));
+	}
+	if($doAction == 'view_excel') {
+		$Label1->SetText(GetCetakExcel($param_arr));
 	}
 // -------------------------
 //End Custom Code
@@ -157,5 +160,109 @@ function GetCetakHTML($param_arr) {
 	$output.='</table>';
 	
 	return $output;
+}
+
+function startExcel($filename = "laporan.xls") {
+    
+	header("Content-type: application/vnd.ms-excel");
+	header("Content-Disposition: attachment; filename=$filename");
+	header("Expires: 0");
+	header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+	header("Pragma: public");
+}
+
+function GetCetakExcel($param_arr) {
+	
+	startExcel("rekap_skpdkb_jabatan.xls");
+	
+	$output = '';
+	
+	$output .='<table id="table-piutang" class="grid-table-container" border="0" cellspacing="0" cellpadding="0">
+          		<tr>
+            		<td valign="top">';
+
+	$output .='<table class="grid-table" border="0" cellspacing="0" cellpadding="0" width="900">
+                	<tr>
+                  		<td class="th"><strong>LAPORAN REKAP SKPDKB</strong></td> 
+                	</tr>
+              		</table>';
+	
+	$output .= '<h2>JENIS PAJAK : '.$param_arr['vat_code'].' </h2>';
+	$output .= '<h2>PERIODE PAJAK : '.$param_arr['code'].'</h2>';
+	$tanggal = CCGetFromGet('date_end_laporan','31-12-2014');
+	$output .='<table id="table-piutang-detil" class="Grid" border="1" cellspacing="0" cellpadding="3px" width="100%">
+                <tr >';
+
+	$output.='<th align="center" >NO</th>';
+	$output.='<th align="center" >NPWPD</th>';
+	$output.='<th align="center" >NAMA</th>';
+	$output.='<th align="center" >ALAMAT</th>';
+	$output.='<th align="center" >JATUH TEMPO</th>';
+	$output.='<th align="center" >MASA PAJAK</th>';
+	$output.='<th align="center" >TGL TAP</th>';
+	$output.='<th align="center" >POKOK (PAJAK TERHUTANG+KENAIKAN)</th>';
+	$output.='<th align="center" >PAJAK TEHUTANG</th>';
+	$output.='<th align="center" >KENAIKAN PAJAK TERHUTANG</th>';
+	$output.='<th align="center" >DENDA</th>';
+	$output.='<th align="center" >TOTAL HARUS DIBAYAR</th>';
+	$output.='<th align="center" >STATUS BAYAR</th>';
+	$output.='<th align="center" >TANGGAL BAYAR</th>';
+	$output.='</tr>';
+	
+	$dbConn	= new clsDBConnSIKP();
+	$query="select a.t_vat_setllement_id as set_id,a.npwd as npwpd ,to_char(start_period,'dd-mm-yyyy')||' s.d. '||to_char(end_period,'dd-mm-yyyy') as masa_pajak,
+		to_char(due_date,'dd-mm-yyyy')as due_date_char, to_char(settlement_date,'dd-mm-yyyy') as tgl_tap,
+		* from t_vat_setllement a
+		left join t_cust_account x on x.t_cust_account_id=a.t_cust_account_id
+		left join t_payment_receipt y on y.t_vat_setllement_id=a.t_vat_setllement_id
+		where p_settlement_type_id = 4 and a.p_finance_period_id =".$param_arr['p_finance_period_id']."
+		and a.p_vat_type_dtl_id in (select p_vat_type_dtl_id from p_vat_type_dtl where p_vat_type_id =".$param_arr['p_vat_type_id'].")
+		and a.p_vat_type_dtl_id not in (11, 15, 41, 12, 42, 43, 30, 17, 21, 27, 31)
+		and x.p_account_status_id = 1
+		and a.npwd ilike '%".$param_arr['npwpd']."%'";
+	if ($param_arr['status_bayar']==2){
+		$query.="and receipt_no is not null ORDER BY wp_name";
+	}else{
+		if ($param_arr['status_bayar']==3){
+			$query.="and receipt_no is null ORDER BY wp_name";
+		}else{
+			$query.="ORDER BY wp_name";
+		}
+	}
+	//echo $query;exit;
+	$data = array();
+	$dbConn->query($query);
+	while ($dbConn->next_record()) {
+		$data[] = $dbConn->Record;
+	}
+	$dbConn->close();
+
+	for ($i = 0; $i < count($data); $i++) {
+		$output.='<tr><td align="center" >'.($i+1).'</td>';
+		$output.='<td align="left" >'.$data[$i]['npwpd'].'</td>';
+		$output.='<td align="left" >'.$data[$i]['wp_name'].'</td>';
+		$output.='<td align="left" >'.$data[$i]['wp_address_name'].'</td>';
+		$output.='<td align="left" >'.$data[$i]['due_date_char'].'</td>';
+		$output.='<td align="left" >'.$data[$i]['masa_pajak'].'</td>';
+		$output.='<td align="left" >'.$data[$i]['tgl_tap'].'</td>';
+		$output.='<td align="right" >'.number_format($data[$i]['total_vat_amount'], 2, ',', '.').'</td>';
+		$output.='<td align="right" >'.number_format($data[$i]['debt_vat_amt'], 2, ',', '.').'</td>';
+		$output.='<td align="right" >'.number_format($data[$i]['db_increasing_charge']+$data[$i]['db_interest_charge'], 2, ',', '.').'</td>';
+		$output.='<td align="right" >'.number_format($data[$i]['total_penalty_amount'], 2, ',', '.').'</td>';
+		$output.='<td align="right" >'.number_format(($data[$i]['total_penalty_amount']+$data[$i]['db_increasing_charge']+$data[$i]['db_interest_charge']+$data[$i]['debt_vat_amt']), 2, ',', '.').'</td>';
+		
+		if ($data[$i]['payment_date']=='') {
+			$output.='<td align="left" >Belum Bayar</td>';
+		}else{
+			$output.='<td align="left" >Sudah Bayar</td>';
+		}
+		$output.='<td align="left" >'.$data[$i]['payment_date'].'</td>';
+		$output.='</tr>';
+	}
+
+	$output.='</table>';
+
+	echo $output;
+	exit;
 }
 ?>
