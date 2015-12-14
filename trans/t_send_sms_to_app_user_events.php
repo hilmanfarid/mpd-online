145f0893ae3e6c7313d97623cd28dd02d07cec7d
wiliamdecosta <wiliamdecosta@gmail.com>
2015-12-14 12:55:30 +0700

    ''

--- trans/t_send_sms_to_app_user_events.php
+++ trans/t_send_sms_to_app_user_events.php
@@ -29,17 +29,15 @@ function grafik_pembayaran_form_Button2_OnClick(& $sender)
 	$query = 	"INSERT INTO t_sms_outbox(
 			            npwpd, mobile_no, message, is_sent, date_added, message_type)
 			    VALUES ('".$app_user_name."', '".$mobile_no."', '".$message."', 'N', sysdate, 'SMS_BLAST')";
+
 	$dbConn->query($query);
-	if ($dbConn->next_record()){
-		echo "<script>
-				alert ('SMS akan segera dikirim.');
-			</script>";
-	}else{
-		echo "<script>
-				alert ('SMS tidak berhasil dikirim.');
-			</script>";
-	}
-	exit;
+	echo '<script>
+				alert ("SMS akan segera dikirim.");
+				location.href="t_send_sms_to_app_user.php";
+			</script>
+			
+			';
+	return;
 // -------------------------
 //End Custom Code
 
