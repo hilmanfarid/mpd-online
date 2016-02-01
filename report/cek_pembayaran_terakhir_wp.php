aa01e53d8a6423d7499610c666683841d1339ef8
fajar125 <fajar.rusius@gmail.com>
2016-02-01 15:57:44 +0700

    ''

--- report/cek_pembayaran_terakhir_wp.php
+++ report/cek_pembayaran_terakhir_wp.php
@@ -8,7 +8,10 @@ $npwpd = CCGetFromGet("npwpd", "");
 
 $dbConn = new clsDBConnSIKP();
 
-$query="select * from t_payment_receipt where npwd = upper('".$npwpd."')
+$query="select * from t_payment_receipt a
+		left join t_cust_account b on a.npwd=b.npwd
+		left join t_customer c on b.t_customer_id = c.t_customer_id
+		where a.npwd = upper('".$npwpd."')
 		ORDER BY payment_date desc limit 1";
 
 $dbConn->query($query);
@@ -28,6 +31,10 @@ if ($dbConn->next_record()) {
 		<npwd>".$dbConn->f("npwd")."</npwd>
 		<payment_vat_amount>".$dbConn->f("payment_vat_amount")."</payment_vat_amount>
 		<finance_period_code>".$dbConn->f("finance_period_code")."</finance_period_code>
+		<merk_dagang>".$dbConn->f("company_brand")."</merk_dagang>
+		<alamat_merk_dagang>".$dbConn->f("brand_address_name")." ".$dbConn->f("brand_address_no")."</alamat_merk_dagang>
+		<pemilik>".$dbConn->f("company_owner")."</pemilik>
+		<alamat_pemilik>".$dbConn->f("address_name_owner")." ".$dbConn->f("address_no_owner")."</alamat_pemilik>
 	</pembayaran_wp>";
 	print_r($myXMLData);
 }else{
