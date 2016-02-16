4bfbd84cf380893f5f2170546c469eb8f0834e06
fajar125 <fajar.rusius@gmail.com>
2016-02-16 15:26:09 +0700

    ''

---
+++ services/pembayaran_wp_bulanan_events.php
@@ -0,0 +1,41 @@
+<?php
+//BindEvents Method @1-CEA43884
+function BindEvents()
+{
+    global $SELECT_target_amt_realisa;
+    $SELECT_target_amt_realisa->ds->CCSEvents["AfterExecuteSelect"] = "SELECT_target_amt_realisa_ds_AfterExecuteSelect";
+}
+//End BindEvents Method
+
+//SELECT_target_amt_realisa_ds_AfterExecuteSelect @2-8CC59EF0
+function SELECT_target_amt_realisa_ds_AfterExecuteSelect(& $sender)
+{
+    $SELECT_target_amt_realisa_ds_AfterExecuteSelect = true;
+    $Component = & $sender;
+    $Container = & CCGetParentContainer($sender);
+    global $SELECT_target_amt_realisa; //Compatibility
+//End SELECT_target_amt_realisa_ds_AfterExecuteSelect
+
+//Custom Code @760-2A29BDB7
+// -------------------------
+    // Write your own code here.
+	$data = array();
+	while($SELECT_target_amt_realisa->DataSource->next_record()){
+		$arr_items = $SELECT_target_amt_realisa->DataSource->Record;
+		$thiskey = $SELECT_target_amt_realisa->DataSource->Record['p_year_period_id'];
+		$data[$thiskey]['arr_data'][]=$arr_items;
+		$data[$thiskey]['amount'][]=(float)$arr_items['pajak'];
+		$data[$thiskey]['tahun']=$arr_items['year_code'];
+	}
+	echo json_encode($data);exit;
+
+// -------------------------
+//End Custom Code
+
+//Close SELECT_target_amt_realisa_ds_AfterExecuteSelect @2-986B12EC
+    return $SELECT_target_amt_realisa_ds_AfterExecuteSelect;
+}
+//End Close SELECT_target_amt_realisa_ds_AfterExecuteSelect
+
+
+?>
