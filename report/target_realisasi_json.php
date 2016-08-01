00a46d0a09b6ffef023943bc8f83394ddf35a803
fajar125 <fajar.rusius@gmail.com>
2016-08-01 11:07:59 +0700

    ''

--- report/target_realisasi_json.php
+++ report/target_realisasi_json.php
@@ -6,14 +6,14 @@ include_once(RelativePath . "/Common.php");
 
 $dbConn = new clsDBConnSIKP();
 
-$query="select * from t_target_realisasi_sementara
+$query="select * from t_target_realisasi_sementara_2
 		ORDER BY p_vat_type_id";
 
 $dbConn->query($query);
-$data=array();
-if ($dbConn->next_record()) {
-	$json = array('items'=>array(),'message'=>'data ditemukan','success'=>1);
-	$item = array(
+$item=array();
+$json = array('items'=>array(),'message'=>'data ditemukan','success'=>1);
+while ($dbConn->next_record()) {	
+	$item[] = array(
 		'p_vat_type_id' => $dbConn->f("p_vat_type_id"),
 		'jenis_pajak' => $dbConn->f("jenis_pajak"),
 		'target_tahunan' => $dbConn->f("target_tahunan"),
@@ -22,26 +22,14 @@ if ($dbConn->next_record()) {
 		'realisasi_triwulan' => $dbConn->f("realisasi_triwulan"),
 		'start_date' => $dbConn->f("start_date"),
 		'end_date' => $dbConn->f("end_date")
-		);
-	$json['items']=$item;
-	$json=json_encode ($json);
-	echo( ($json));
-}else{
-	$json = array('items'=>array(),'message'=>'data tidak ditemukan','success'=>0);
-	$item = array(
-		'p_vat_type_id' => $dbConn->f("p_vat_type_id"),
-		'jenis_pajak' => $dbConn->f("jenis_pajak"),
-		'target_tahunan' => $dbConn->f("target_tahunan"),
-		'target_triwulan' => $dbConn->f("target_triwulan"),
-		'realisasi_tahunan' => $dbConn->f("realisasi_tahunan"),
-		'realisasi_triwulan' => $dbConn->f("realisasi_triwulan"),
-		'start_date' => $dbConn->f("start_date"),
-		'end_date' => $dbConn->f("end_date")
-		);
-	$json['items']=$item;
-	$json=json_encode ($json);
-	echo( ($json));
+		);	
 }
+if (empty($item)) {
+	$json = array('items'=>array(),'message'=>'data tidak ditemukan','success'=>0);
+}
+$json['items']=$item;
+$json=json_encode ($json);
+echo( ($json));
 $dbConn->close();
 
 
